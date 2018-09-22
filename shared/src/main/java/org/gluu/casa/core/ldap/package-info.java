8dfa1e4aa3e8c85269767b31be53e658d8bb7967
Jose G <bonustrack310@gmail.com>
2018-09-22 08:01:19 -0500

    Misc changes

--- shared/src/main/java/org/gluu/casa/core/ldap/package-info.java
+++ shared/src/main/java/org/gluu/casa/core/ldap/package-info.java
@@ -4,6 +4,9 @@
  * Copyright (c) 2018, Gluu
  */
 /**
- * Contains extension points (Java interfaces) that Gluu Casa plugin writers can use to extend application functionality.
+ * Classes generated using the <code>generate-source-from-schema</code> tool of UnboundID LDAP SDK
+ * <a href="https://docs.ldap.com/ldap-sdk/docs/persist/index.html" target="_blank">persistence framework</a>.
+ * <p><b>Plugin writers</b> may extend this classes or generate their own POJOs if those provided here are not enough
+ * for their needs.</p>
  */
 package org.gluu.casa.core.ldap;
--- shared/src/main/java/org/gluu/casa/extension/package-info.java
+++ shared/src/main/java/org/gluu/casa/extension/package-info.java
@@ -4,9 +4,6 @@
  * Copyright (c) 2018, Gluu
  */
 /**
- * Classes generated using the <code>generate-source-from-schema</code> tool of UnboundID LDAP SDK
- * <a href="https://docs.ldap.com/ldap-sdk/docs/persist/index.html" target="_blank">persistence framework</a>.
- * <p><b>Plugin writers</b> may extend this classes or generate their own POJOs if those provided here are not enough
- * for their needs.</p>
+ * Contains extension points (Java interfaces) that Gluu Casa plugin writers can use to extend application functionality.
  */
 package org.gluu.casa.extension;
