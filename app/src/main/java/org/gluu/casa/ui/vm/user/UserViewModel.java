d1e470bede2cb21d42835578cf60497d7ff20d58
Jose G <bonustrack310@gmail.com>
2019-01-19 08:24:52 -0500

    Skip checks on credential removal when user has no preference set

--- app/src/main/java/org/gluu/casa/ui/vm/user/UserViewModel.java
+++ app/src/main/java/org/gluu/casa/ui/vm/user/UserViewModel.java
@@ -10,6 +10,7 @@ import org.gluu.casa.core.*;
 import org.gluu.casa.core.pojo.BrowserInfo;
 import org.gluu.casa.core.pojo.User;
 import org.gluu.casa.extension.AuthnMethod;
+import org.gluu.casa.misc.Utils;
 import org.gluu.casa.ui.CredRemovalConflict;
 import org.zkoss.bind.annotation.Init;
 import org.zkoss.util.Pair;
@@ -96,34 +97,39 @@ public class UserViewModel {
 
         //Assume removal has no problem
         String message = null;
-        List<Pair<AuthnMethod, Integer>> userMethodsCount = userService.getUserMethodsCount(user.getId());
-        int totalCreds = userMethodsCount.stream().mapToInt(Pair::getY).sum();
-        int minCredsFor2FA = confSettings.getMinCredsFor2FA();
+        String preferredMethod = user.getPreferredMethod();
+        if (Utils.isNotEmpty(preferredMethod)) {
 
-        if (nCredsOfType == 1) {
-            List<AuthnMethod> methods = userService.get2FARequisiteMethods();
-            boolean typeOfCredIs2FARequisite = methods.stream().map(AuthnMethod::getAcr).anyMatch(acr -> acr.equals(credentialType));
+            //Compute how many credentials current user has added
+            List<Pair<AuthnMethod, Integer>> userMethodsCount = userService.getUserMethodsCount(user.getId());
+            int totalCreds = userMethodsCount.stream().mapToInt(Pair::getY).sum();
+            int minCredsFor2FA = confSettings.getMinCredsFor2FA();
 
-            if (typeOfCredIs2FARequisite) {
-                //Discard if credential being removed is the only registered that belongs to 2FARequisiteMethods
-                int nCredsBelongingTo2FARequisite = userMethodsCount.stream()
-                        .filter(pair -> pair.getX().mayBe2faActivationRequisite()).mapToInt(Pair::getY).sum();
-                if (nCredsBelongingTo2FARequisite == 1) {
-                    //Compute the names of those authentication methods which are requisite for 2FA activation
-                    String commaSepNames = methods.stream().map(aMethod -> Labels.getLabel(aMethod.getUINameKey()))
-                            .collect(Collectors.toList()).toString();
-                    commaSepNames = commaSepNames.substring(1, commaSepNames.length() - 1);
-                    message = CredRemovalConflict.REQUISITE_NOT_FULFILED.getMessage(commaSepNames);
+            if (nCredsOfType == 1) {
+                List<AuthnMethod> methods = userService.get2FARequisiteMethods();
+                boolean typeOfCredIs2FARequisite = methods.stream().map(AuthnMethod::getAcr).anyMatch(acr -> acr.equals(credentialType));
+
+                if (typeOfCredIs2FARequisite) {
+                    //Check if credential being removed is the only one belonging to 2FARequisiteMethods
+                    int nCredsBelongingTo2FARequisite = userMethodsCount.stream()
+                            .filter(pair -> pair.getX().mayBe2faActivationRequisite()).mapToInt(Pair::getY).sum();
+                    if (nCredsBelongingTo2FARequisite == 1) {
+                        //Compute the names of those authentication methods which are requisite for 2FA activation
+                        String commaSepNames = methods.stream().map(aMethod -> Labels.getLabel(aMethod.getUINameKey()))
+                                .collect(Collectors.toList()).toString();
+                        commaSepNames = commaSepNames.substring(1, commaSepNames.length() - 1);
+                        message = CredRemovalConflict.REQUISITE_NOT_FULFILED.getMessage(commaSepNames);
+                    }
+                } else if (credentialType.equals(preferredMethod)) {
+                    message = CredRemovalConflict.PREFERRED_CREDENTIAL_REMOVED.getMessage();
                 }
-            } else if (credentialType.equals(user.getPreferredMethod())) {
-                message = CredRemovalConflict.PREFERRED_CREDENTIAL_REMOVED.getMessage();
+            }
+            if (message == null && totalCreds == minCredsFor2FA) {
+                message = CredRemovalConflict.CREDS2FA_NUMBER_UNDERFLOW.getMessage(minCredsFor2FA);
             }
         }
-        if (message == null && totalCreds == minCredsFor2FA) {
-            message = CredRemovalConflict.CREDS2FA_NUMBER_UNDERFLOW.getMessage(minCredsFor2FA);
-        }
         if (message != null) {
-            message = Labels.getLabel("usr.del_conflict_revert", new String[]{ message });
+            message = Labels.getLabel("usr.del_conflict_revert", new String[]{message});
         }
         return message;
 
