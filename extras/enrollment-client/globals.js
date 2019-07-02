9fc477f2b1bfcaeb2af2c32f3dd7304713012943
Jose G <bonustrack310@gmail.com>
2019-07-02 15:13:42 -0500

    js adjustment #35

--- extras/enrollment-client/globals.js
+++ extras/enrollment-client/globals.js
@@ -1,14 +1,14 @@
 //Defines some global variables
 
 //The URL of a server where Casa is installed e.g. https://my.company.com
-var issuer = "https://my.company.com"
+var issuer = "https://some.gluu.info"
 var OIDCUrl = issuer + "/.well-known/openid-configuration"
 var endpoints_root = issuer + "/casa/rest/enrollment"
 
 //This client should exist already in the Gluu server pointed above
 //Ensure you add the domain you are using to serve these pages in the "Add authorized Javascript origin" text field
-var clientId= "@!..."
-var clientSecret = "change it"
+var clientId= "0911eac5-41ec-425c-857b-d92927647028"
+var clientSecret = "secret"
 
 //leave this blank
 var token=""
