GluuFederation/flex

commit e2177c77b1415ae851ed04658b81601a13fc88cd

Small refactoring of oxd settings parsing code

app/src/main/java/org/gluu/casa/core/ConfigurationHandler.java
@@ -3,6 +3,7 @@ package org.gluu.casa.core;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.gluu.casa.conf.MainSettings;
+import org.gluu.casa.conf.OxdSettings;
 import org.gluu.casa.conf.sndfactor.EnforcementPolicy;
 import org.gluu.casa.misc.AppStateEnum;
 import org.gluu.casa.misc.Utils;
@@ -148,7 +149,8 @@ public class ConfigurationHandler extends JobListenerSupport {
                     computePassResetable();
                     compute2FAEnforcementPolicy();
 
-                    if (oxdService.initialize(settings.getOxdSettings(true))) {
+                    OxdSettings oxdSettings = Optional.ofNullable(settings.getOxdSettings()).orElse(settings.getOxdSettings(true));
+                    if (oxdService.initialize(oxdSettings)) {
                         extManager.scan();
                         computeAcrPluginMapping();
 

app/src/main/java/org/gluu/casa/core/OxdService.java
@@ -88,12 +88,7 @@ public class OxdService {
                 try {
                     if (persistenceService.getDynamicClientExpirationTime() != 0) {
                         Optional<String> oxdIdOpt = Optional.ofNullable(oxdConfig.getClient()).map(OxdClientSettings::getOxdId);
-                        if (oxdIdOpt.isPresent()) {
-                            setSettings(oxdConfig);
-                        } else {
-                            //trigger registration
-                            setSettings(oxdConfig, true);
-                        }
+                        setSettings(oxdConfig, !oxdIdOpt.isPresent());
                         success = true;
                     } else {
                         logger.error("Dynamic registration of OpenId Connect clients must be enabled in the server.");
@@ -106,10 +101,6 @@ public class OxdService {
         }
         return success;
 
-    }
-
-    public void setSettings(OxdSettings config) throws Exception {
-        setSettings(config, false);
     }
 
     public void setSettings(OxdSettings config, boolean triggerRegistration) throws Exception {

commit edf1034414d37d1bc54c82954be768df85da5ab5

Fix bug introduced by commit https://github.com/GluuFederation/casa/commit/e2177c77b1415ae851ed04658b81601a13fc88cd

app/src/main/java/org/gluu/casa/core/ConfigurationHandler.java
@@ -104,7 +104,7 @@ public class ConfigurationHandler extends JobListenerSupport {
                 /*
                  A gap of 5 seconds is enough for the RestEasy scanning process to take place (in case oxAuth is already up and running)
                  RETRIES*RETRY_INTERVAL seconds gives room to recover the acr list. This big amount of time may be required
-                 in cases where cred-manager service starts too soon (even before oxAuth itself)
+                 in cases where casa service starts too soon (even before oxAuth itself)
                 */
                 timerService.schedule(acrQuartzJobName, 5, RETRIES, RETRY_INTERVAL);
             } else {
@@ -149,7 +149,7 @@ public class ConfigurationHandler extends JobListenerSupport {
                     computePassResetable();
                     compute2FAEnforcementPolicy();
 
-                    OxdSettings oxdSettings = Optional.ofNullable(settings.getOxdSettings()).orElse(settings.getOxdSettings(true));
+                    OxdSettings oxdSettings = settings.getOxdSettings(true);
                     if (oxdService.initialize(oxdSettings)) {
                         extManager.scan();
                         computeAcrPluginMapping();

app/src/main/java/org/gluu/casa/core/OxdService.java
@@ -70,6 +70,7 @@ public class OxdService {
         client = RSUtils.getClient();
     }
 
+    //This method modifies the param supplied
     public boolean initialize(OxdSettings oxdConfig) {
 
         boolean success = false;
@@ -96,6 +97,7 @@ public class OxdService {
                 } catch (Exception e) {
                     logger.warn("Users will not be able to login until a new sucessful attempt to refresh oxd-associated "
                             + "clients takes place. Restart the app to trigger the update immediately");
+                    logger.error(e.getMessage());
                 }
             }
         }
@@ -103,11 +105,13 @@ public class OxdService {
 
     }
 
-    public void setSettings(OxdSettings config, boolean triggerRegistration) throws Exception {
-        this.config = config;
+    public void setSettings(OxdSettings cfg, boolean triggerRegistration) throws Exception {
+
+        this.config = cfg;
         if (triggerRegistration) {
-            config.setClient(doRegister());
+            cfg.setClient(doRegister());
         }
+
     }
 
     public OxdClientSettings doRegister() throws Exception {