ff53d4c2bb4ac2f274ed5fa4a70526b4e35a98f9
Jose G <bonustrack310@gmail.com>
2019-12-05 13:53:37 -0500

    Prepare for future 2fa changes, #103

--- app/src/main/java/org/gluu/casa/core/plugin/PluginSettingsHandler.java
+++ app/src/main/java/org/gluu/casa/core/plugin/PluginSettingsHandler.java
@@ -5,14 +5,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.gluu.casa.core.ConfigurationHandler;
 import org.gluu.casa.misc.Utils;
 import org.gluu.casa.service.settings.IPluginSettingsHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.Map;
+import java.util.Optional;
 
 public class PluginSettingsHandler<T> implements IPluginSettingsHandler<T> {
 
     private static ConfigurationHandler configurationHandler = Utils.managedBean(ConfigurationHandler.class);
     private static ObjectMapper mapper = new ObjectMapper();
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
     private String pluginKey;
 
     public PluginSettingsHandler(String pluginKey) {
@@ -20,17 +24,33 @@ public class PluginSettingsHandler<T> implements IPluginSettingsHandler<T> {
     }
 
     public void save() throws Exception {
+/*
+        //Hack for happy 2fa plugin
+        try {
+            if (pluginKey.equals("strong-authn-settings")) {
+                Optional.ofNullable(getSettingsMap()).map(m -> m.get("min_creds_2FA")).map(Integer.class::cast)
+                        .ifPresent(configurationHandler.getSettings()::setMinCredsFor2FA);
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        } //hack end
+*/
         configurationHandler.saveSettings();
+
     }
 
     public T getSettings() {
-        Map<String, Object> map = configurationHandler.getSettings().getPluginSettings().get(pluginKey);
+        Map<String, Object> map = getSettingsMap();
         return map == null ? null : mapper.convertValue(map, new TypeReference<T>(){});
     }
 
     public void setSettings(T settings) {
         Map<String, Object> map = mapper.convertValue(settings, new TypeReference<Map<String, Object>>(){});
         configurationHandler.getSettings().getPluginSettings().put(pluginKey, map);
+    }
+
+    private Map<String, Object> getSettingsMap() {
+        return configurationHandler.getSettings().getPluginSettings().get(pluginKey);
     }
 
 }
