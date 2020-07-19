5422572b7c4aafe03d3d25bf69c0fa70737669f8
iromli <isman.firmansyah@gmail.com>
2020-07-20 05:50:22 +0700

    implement upsert-like query to manage casa configuration in persistence; fix #4

--- scripts/casa_config.py
+++ scripts/casa_config.py
@@ -1,7 +1,14 @@
 import os
 import json
+from collections import namedtuple
 
-import ldap3
+from ldap3 import (
+    BASE,
+    Connection,
+    MODIFY_DELETE,
+    MODIFY_REPLACE,
+    Server,
+)
 
 from pygluu.containerlib.persistence.couchbase import CouchbaseClient
 from pygluu.containerlib.persistence.couchbase import get_couchbase_password
@@ -10,76 +17,132 @@ from pygluu.containerlib.utils import decode_text
 
 from oxd import resolve_oxd_url
 
+Entry = namedtuple("Entry", ["id", "attrs"])
 
-class LDAPBackend(object):
-    key = "ou=casa,ou=configuration,o=gluu"
 
+class LDAPBackend:
     def __init__(self, manager):
-        host = os.environ.get("GLUU_LDAP_URL", "localhost:1636")
+        url = os.environ.get("GLUU_LDAP_URL", "localhost:1636")
         user = manager.config.get("ldap_binddn")
-        password = decode_text(
+        passwd = decode_text(
             manager.secret.get("encoded_ox_ldap_pw"),
             manager.secret.get("encoded_salt"),
         )
 
-        server = ldap3.Server(host, port=1636, use_ssl=True)
-        self.conn = ldap3.Connection(server, user, password)
-        # self.manager = manager
+        server = Server(url, port=1636, use_ssl=True)
+        self.conn = Connection(server, user, passwd)
+        self.manager = manager
 
-    def add_config(self, data):
-        with self.conn as conn:
-            conn.add(
-                self.key,
-                attributes={
-                    "objectClass": ["top", "oxApplicationConfiguration"],
-                    "ou": "casa",
-                    "oxConfApplication": json.dumps(data),
-                },
-            )
-            return bool(conn.result["result"] == 0)
+    def get_entry(self, key, filter_="", attrs=None, **kwargs):
+        attrs = None or ["*"]
+        filter_ = filter_ or "(objectClass=*)"
 
-    def config_exists(self):
         with self.conn as conn:
             conn.search(
-                search_base=self.key,
-                search_filter="(objectClass=oxApplicationConfiguration)",
-                search_scope=ldap3.BASE,
-                attributes=['objectClass'],
-                size_limit=1,
+                search_base=key,
+                search_filter=filter_,
+                search_scope=BASE,
+                attributes=attrs,
             )
-            return bool(conn.entries)
+
+            if not conn.entries:
+                return
+
+            entry = conn.entries[0]
+            id_ = entry.entry_dn
+            attrs = {}
+
+            for k, v in entry.entry_attributes_as_dict.items():
+                if len(v) < 2:
+                    v = v[0]
+                attrs[k] = v
+            return Entry(id_, attrs)
+
+    def modify_entry(self, key, attrs=None, **kwargs):
+        attrs = attrs or {}
+        del_flag = kwargs.get("delete_attr", False)
+
+        if del_flag:
+            mod = MODIFY_DELETE
+        else:
+            mod = MODIFY_REPLACE
+
+        for k, v in attrs.items():
+            if not isinstance(v, list):
+                v = [v]
+            attrs[k] = [(mod, v)]
+
+        with self.conn as conn:
+            conn.modify(key, attrs)
+            return bool(conn.result["description"] == "success"), conn.result["message"]
+
+    def add_entry(self, key, attrs=None, **kwargs):
+        attrs = attrs or {}
+
+        for k, v in attrs.items():
+            if not isinstance(v, list):
+                v = [v]
+            attrs[k] = v
+
+        with self.conn as conn:
+            conn.add(key, attributes=attrs)
+            return bool(conn.result["description"] == "success"), conn.result["message"]
 
 
-class CouchbaseBackend(object):
-    key = "configuration_casa"
-
+class CouchbaseBackend:
     def __init__(self, manager):
-        host = os.environ.get("GLUU_COUCHBASE_URL", "localhost")
+        hosts = os.environ.get("GLUU_COUCHBASE_URL", "localhost")
         user = get_couchbase_user(manager)
         password = get_couchbase_password(manager)
-        self.client = CouchbaseClient(host, user, password)
+        self.client = CouchbaseClient(hosts, user, password)
 
-    def add_config(self, data):
-        attributes = json.dumps({
-            "dn": "ou=casa,ou=configuration,o=gluu",
-            "objectClass": "oxApplicationConfiguration",
-            "ou": "casa",
-            "oxConfApplication": data,
-        })
+    def get_entry(self, key, filter_="", attrs=None, **kwargs):
+        bucket = kwargs.get("bucket")
+        req = self.client.exec_query(
+            "SELECT META().id, {0}.* FROM {0} USE KEYS '{1}'".format(bucket, key)
+        )
 
-        query = 'INSERT INTO `%s` (KEY, VALUE) VALUES ("%s", %s);\n' % ("gluu", self.key, attributes)
+        if not req.ok:
+            return
+
+        try:
+            attrs = req.json()["results"][0]
+            return Entry(attrs.pop("id"), attrs)
+        except IndexError:
+            return
+
+    def modify_entry(self, key, attrs=None, **kwargs):
+        attrs = attrs or {}
+        bucket = kwargs.get("bucket")
+        del_flag = kwargs.get("delete_attr", False)
+
+        if del_flag:
+            mod_kv = "UNSET {}".format(
+                ",".join([k for k, _ in attrs.items()])
+            )
+        else:
+            mod_kv = "SET {}".format(
+                ",".join(["{}={}".format(k, json.dumps(v)) for k, v in attrs.items()])
+            )
+
+        query = "UPDATE {} USE KEYS '{}' {}".format(bucket, key, mod_kv)
+        req = self.client.exec_query(query)
+        if req.ok:
+            resp = req.json()
+            return bool(resp["status"] == "success"), resp["status"]
+        return False, ""
+
+    def add_entry(self, key, attrs=None, **kwargs):
+        attrs = attrs or {}
+        bucket = kwargs.get("bucket")
+
+        query = 'INSERT INTO `%s` (KEY, VALUE) VALUES ("%s", %s);\n' % (bucket, key, json.dumps(attrs))
         req = self.client.exec_query(query)
 
         if req.ok:
-            return req.json()["status"] == "success"
-        return False
-
-    def config_exists(self):
-        query = "SELECT objectClass FROM {0} USE KEYS '{1}'".format("gluu", self.key)
-        req = self.client.exec_query(query)
-        if req.ok:
-            return bool(req.json()["results"])
-        return False
+            resp = req.json()
+            return bool(resp["status"] == "success"), resp["status"]
+        return False, ""
 
 
 class CasaConfig(object):
@@ -90,14 +153,20 @@ class CasaConfig(object):
         ldap_mapping = os.environ.get("GLUU_PERSISTENCE_LDAP_MAPPING", "default")
 
         if persistence_type == "ldap":
+            backend_type = "ldap"
             backend_cls = LDAPBackend
         elif persistence_type == "couchbase":
+            backend_type = "couchbase"
             backend_cls = CouchbaseBackend
         else:  # probably `hybrid`
             if ldap_mapping == "default":
+                backend_type = "ldap"
                 backend_cls = LDAPBackend
             else:
+                backend_type = "couchbase"
                 backend_cls = CouchbaseBackend
+
+        self.backend_type = backend_type
         self.backend = backend_cls(self.manager)
 
     def json_from_template(self):
@@ -116,6 +185,55 @@ class CasaConfig(object):
             return json.loads(fr.read() % ctx)
 
     def setup(self):
-        if not self.backend.config_exists():
-            data = self.json_from_template()
-            self.backend.add_config(data)
+        data = self.json_from_template()
+
+        if self.backend_type == "ldap":
+            key = "ou=casa,ou=configuration,o=gluu"
+        else:
+            key = "configuration_casa"
+
+        config = self.backend.get_entry(key, **{"bucket": "gluu"})
+
+        if not config:
+            conf_app = data
+
+            if self.backend_type == "ldap":
+                attrs = {
+                    "objectClass": ["top", "oxApplicationConfiguration"],
+                    "ou": "casa",
+                    "oxConfApplication": json.dumps(data),
+                }
+            else:
+                attrs = {
+                    "dn": "ou=casa,ou=configuration,o=gluu",
+                    "objectClass": "oxApplicationConfiguration",
+                    "ou": "casa",
+                    "oxConfApplication": data,
+                }
+
+            self.backend.add_entry(key, attrs, **{"bucket": "gluu"})
+
+        # if config exists, modify it if neccessary
+        else:
+            should_modify = False
+
+            # compare oxd_config
+            conf_app = config.attrs["oxConfApplication"]
+            if self.backend_type == "ldap":
+                conf_app = json.loads(conf_app)
+
+            if data["oxd_config"]["host"] != conf_app["oxd_config"]["host"]:
+                conf_app["oxd_config"]["host"] = data["oxd_config"]["host"]
+                should_modify = True
+
+            if data["oxd_config"]["port"] != conf_app["oxd_config"]["port"]:
+                conf_app["oxd_config"]["port"] = data["oxd_config"]["port"]
+                should_modify = True
+
+            if not should_modify:
+                return
+
+            if self.backend_type == "ldap":
+                conf_app = json.dumps(conf_app)
+            attrs = {"oxConfApplication": conf_app}
+            self.backend.modify_entry(config.id, attrs, **{"bucket": "gluu"})
