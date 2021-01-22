005b05cb21a42abcda5ebd5237fcc9c7c5ef69de
Gasmyr <thomas@gluu.org>
2021-01-22 12:17:23 +0100

    Create a base class for default api client

--- app/redux/api/attribute-api.js
+++ app/redux/api/attribute-api.js
@@ -1,28 +1,7 @@
+import { getDefaultClient } from "./base";
 const JansConfigApi = require("jans_config_api");
-const defaultClient = JansConfigApi.ApiClient.instance;
-defaultClient.timeout = 50000;
-const jansauth = defaultClient.authentications["jans-auth"];
-defaultClient.basePath = "https://gasmyr.gluu.org".replace(/\/+$/, "");
-const headers = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers":
-    "Origin, X-Requested-With, Content-Type, Accept",
-  "Content-Type": "application/json",
-  "Access-Control-Allow-Credentials": true
-};
-defaultClient.defaultHeaders = headers;
-
-function getApiAccessToken() {
-  console.log(
-    "==============Fetching the api token from store " +
-      localStorage.getItem("gluu.api.token")
-  );
-  return localStorage.getItem("gluu.api.token");
-}
-jansauth.accessToken = getApiAccessToken();
-console.log(" ===== attribute-api.js::::jansauth.accessToken = "+jansauth.accessToken+" ===== ");
-
-const api = new JansConfigApi.AttributeApi();
+const defaultClient = getDefaultClient(JansConfigApi);
+const api = new JansConfigApi.AttributeApi(defaultClient);
 
 // Get all attributes
 export const getAllAttributes = () => {
@@ -31,11 +10,13 @@ export const getAllAttributes = () => {
       if (error) {
         reject(error);
       } else {
-       	  console.log(" ==========================********** getAllAttributes() data = "+data+" **********========================== ");
+        console.log(
+          "********** getAllAttributes() data = " +
+            data +
+            " **********========================== "
+        );
         resolve(data);
       }
     });
   });
 };
-
-
--- app/redux/api/scope-api.js
+++ app/redux/api/scope-api.js
@@ -1,25 +1,7 @@
+import { getDefaultClient } from "./base";
 const JansConfigApi = require("jans_config_api");
-const defaultClient = JansConfigApi.ApiClient.instance;
-defaultClient.timeout = 50000;
-const jansauth = defaultClient.authentications["jans-auth"];
-defaultClient.basePath = "https://gasmyr.gluu.org".replace(/\/+$/, "");
-const headers = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers":
-    "Origin, X-Requested-With, Content-Type, Accept",
-  "Content-Type": "application/json",
-  "Access-Control-Allow-Credentials": true
-};
-defaultClient.defaultHeaders = headers;
-function getApiAccessToken() {
-  console.log(
-    "==============Fetching the api token from store " +
-      localStorage.getItem("gluu.api.token")
-  );
-  return localStorage.getItem("gluu.api.token");
-}
-jansauth.accessToken = getApiAccessToken();
-const api = new JansConfigApi.OAuthScopesApi();
+const defaultClient = getDefaultClient(JansConfigApi);
+const api = new JansConfigApi.OAuthScopesApi(defaultClient);
 
 // Get All scopes
 export const getAllScopes = () => {
