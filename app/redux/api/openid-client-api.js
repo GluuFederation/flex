bb16376949658b214e4a00aa76bbe8c7138f342c
pujavs <pujas.works@gmail.com>
2021-01-22 17:28:37 +0530

    Code refactor incorporated

--- app/redux/api/openid-client-api.js
+++ app/redux/api/openid-client-api.js
@@ -1,27 +1,6 @@
+import { getDefaultClient } from "./base";
 const JansConfigApi = require("jans_config_api");
-const defaultClient = JansConfigApi.ApiClient.instance;
-defaultClient.timeout = 50000;
-const jansauth = defaultClient.authentications["jans-auth"];
-defaultClient.basePath = "https://gasmyr.gluu.org".replace(/\/+$/, "");
-const headers = {
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Headers":
-    "Origin, X-Requested-With, Content-Type, Accept",
-  "Content-Type": "application/json",
-  "Access-Control-Allow-Credentials": true
-};
-defaultClient.defaultHeaders = headers;
-
-function getApiAccessToken() {
-  console.log(
-    "==============Fetching the api token from store " +
-      localStorage.getItem("gluu.api.token")
-  );
-  return localStorage.getItem("gluu.api.token");
-}
-jansauth.accessToken = getApiAccessToken();
-
-const api = new JansConfigApi.OAuthOpenIDConnectClientsApi();
+const api = new JansConfigApi.OAuthOpenIDConnectClientsApi(getDefaultClient(JansConfigApi));
 
 // Get all Openid clients
 export const getAllOpenidClients = () => {
