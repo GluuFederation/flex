c620778aa6e94fc0fb72b3c1d349bc9c8b94fbaa
Thomas Gasmyr <Thomas@gluu.org>
2021-02-14 19:30:14 +0100

    Remove useless components

--- app/i18n.js
+++ app/i18n.js
@@ -1,6 +1,6 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
 i18n
   .use(initReactI18next)
   .use(LanguageDetector)
@@ -9,26 +9,26 @@ i18n
     resources: {
       en: {
         translation: {
-          french: "French",
-          english: "English"
-        }
+          french: 'French',
+          english: 'English',
+        },
       },
       fr: {
         translation: {
-          french: "Français",
-          english: "Anglais"
-        }
-      }
+          french: 'Français',
+          english: 'Anglais',
+        },
+      },
     },
-    fallbackLng: "en",
+    fallbackLng: 'en',
     debug: true,
     // have a common namespace used around the full app
-    ns: ["translation"],
-    defaultNS: "translation",
+    ns: ['translation'],
+    defaultNS: 'translation',
     keySeparator: false, // we use content as keys
     interpolation: {},
     react: {
-      wait: true
-    }
-  });
-export default i18n;
+      wait: true,
+    },
+  })
+export default i18n
