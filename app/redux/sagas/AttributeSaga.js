f7056e59f45302d6ed8c1ec1a3dfb69172bbba22
Thomas Gasmyr <Thomas@gluu.org>
2021-03-08 18:26:05 +0100

    Fix page refresh issue for Attributes

--- app/redux/sagas/AttributeSaga.js
+++ app/redux/sagas/AttributeSaga.js
@@ -4,12 +4,6 @@
 import { call, all, put, fork, takeLatest, select } from 'redux-saga/effects'
 import { isFourZeroOneError, hasApiToken } from '../../utils/TokenController'
 import {
-  getAllAttributes,
-  addNewAttribute,
-  editAnAttribute,
-  deleteAnAttribute,
-} from '../api/attribute-api'
-import {
   getAttributesResponse,
   addAttributeResponse,
   editAttributeResponse,
@@ -26,18 +20,10 @@ import {
 import AttributeApi from '../api/AttributeApi'
 import { getClient } from '../api/base'
 const JansConfigApi = require('jans_config_api')
-const kapi = new JansConfigApi.AttributeApi(
-  getClient(JansConfigApi, 'token', 'issuer'),
-)
 
 export function* getAttributes() {
   try {
-    const token = yield select((state) => state.authReducer.token.access_token)
-    const issuer = yield select((state) => state.authReducer.issuer)
-    const api = new JansConfigApi.AttributeApi(
-      getClient(JansConfigApi, token, issuer),
-    )
-    const attributeApi = new AttributeApi(api)
+    const attributeApi = yield* newFunction()
     const data = yield call(attributeApi.getAllAttributes)
     yield put(getAttributesResponse(data))
   } catch (e) {
@@ -50,7 +36,8 @@ export function* getAttributes() {
 
 export function* addAttribute({ payload }) {
   try {
-    const data = yield call(addNewAttribute, payload.data)
+    const attributeApi = yield* newFunction()
+    const data = yield call(attributeApi.addNewAttribute, payload.data)
     yield put(addAttributeResponse(data))
   } catch (error) {
     yield put(setApiError(error))
@@ -59,7 +46,8 @@ export function* addAttribute({ payload }) {
 
 export function* editAttribute({ payload }) {
   try {
-    const data = yield call(editAnAttribute, payload.data)
+    const attributeApi = yield* newFunction()
+    const data = yield call(attributeApi.editAnAttribute, payload.data)
     yield put(editAttributeResponse(data))
   } catch (error) {
     yield put(setApiError(error))
@@ -68,11 +56,21 @@ export function* editAttribute({ payload }) {
 
 export function* deleteAttribute({ payload }) {
   try {
-    const data = yield call(deleteAnAttribute, payload.data)
+    const attributeApi = yield* newFunction()
+    const data = yield call(attributeApi.deleteAnAttribute, payload.data)
     yield put(deleteAttributeResponse(data))
   } catch (error) {
     yield put(setApiError(error))
   }
+}
+
+function* newFunction() {
+  const token = yield select((state) => state.authReducer.token.access_token)
+  const issuer = yield select((state) => state.authReducer.issuer)
+  const api = new JansConfigApi.AttributeApi(
+    getClient(JansConfigApi, token, issuer),
+  )
+  return new AttributeApi(api)
 }
 
 export function* watchGetAttributes() {
