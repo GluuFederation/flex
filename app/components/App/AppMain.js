8b3372abd86bf106b9c00ffe0a94053018532edd
Arnab Dutta <arnab.bdutta@gmail.com>
2021-03-15 14:40:15 +0530

    #31 - Log out feature in admin-ui

--- app/components/App/AppMain.js
+++ app/components/App/AppMain.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
-import AppLayout from '../../layout/default'
-import { RoutedContent } from '../../routes'
-import AppAuthProvider from '../../utils/AppAuthProvider'
 import { Provider } from 'react-redux'
 import { configureStore } from '../../redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
+import AuthenticatedRouteSelector from './AuthenticatedRouteSelector'
 const basePath = process.env.BASE_PATH || '/'
 
 const AppMain = () => {
@@ -14,11 +12,7 @@ const AppMain = () => {
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <Router basename={basePath}>
-          <AppAuthProvider>
-            <AppLayout>
-              <RoutedContent />
-            </AppLayout>
-          </AppAuthProvider>
+        <AuthenticatedRouteSelector/>
         </Router>
       </PersistGate>
     </Provider>
