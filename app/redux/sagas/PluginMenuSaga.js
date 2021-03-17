3814d2083a850f8fe36b2091ec7738ff565f1296
Thomas Gasmyr <Thomas@gluu.org>
2021-03-17 10:50:10 +0100

    Fix error un Plugin menu saga

--- app/redux/sagas/PluginMenuSaga.js
+++ app/redux/sagas/PluginMenuSaga.js
@@ -3,17 +3,11 @@
  */
 
 import { all, call, fork, put, takeEvery } from 'redux-saga/effects'
-import {
-  GET_ALL_PLUGIN_MENU,
-} from '../actions/types'
+import { GET_ALL_PLUGIN_MENU } from '../actions/types'
 
-import {
-	getAllPluginResponse,
-} from '../actions/PluginMenuActions'
+import { getAllPluginResponse } from '../actions/PluginMenuActions'
 
-import {
-  fetchPluginList,
-} from '../api/plugin-menu-api'
+import { fetchPluginList } from '../api/plugin-menu-api'
 
 function* getPluginWorker() {
   try {
@@ -28,18 +22,14 @@ function* getPluginWorker() {
   yield put(getAllPluginsResponse())
 }
 
-
 //watcher sagas
 export function* getPluginWatcher() {
   yield takeEvery(GET_ALL_PLUGIN_MENU, getPluginWorker)
 }
 
-
 /**
  * Plugin Root Saga
  */
 export default function* rootSaga() {
-  yield all([
-    fork(getPluginWatcher),
-  ])
+  yield all([fork(getPluginWatcher)])
 }
