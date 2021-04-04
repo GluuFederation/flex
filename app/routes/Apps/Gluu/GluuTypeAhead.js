154377eb00fcebeda2bde74e076d3c93ceb1cf6c
Arnab Dutta <arnab.bdutta@gmail.com>
2021-04-04 17:55:55 +0530

    feat: add field instruction in GluuTypeAhead

--- app/routes/Apps/Gluu/GluuTypeAhead.js
+++ app/routes/Apps/Gluu/GluuTypeAhead.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import { FormGroup,Col } from '../../../components'
+import { FormGroup, Col } from '../../../components'
 import { Typeahead } from 'react-bootstrap-typeahead'
 import GluuLabel from '../Gluu/GluuLabel'
+import Typography from '@material-ui/core/Typography';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  typography: {
+    subtitle1: {
+      fontSize: 12,
+    },
+  },
+});
 
 function GluuTypeAhead({ label, name, value, options, formik, required }) {
- // console.log("========name: "+name+ " and value: "+value)
+  // console.log("========name: "+name+ " and value: "+value)
   return (
     <FormGroup row>
       {!!required ? <GluuLabel label={label} size={4} required /> : <GluuLabel label={label} size={4} />}
@@ -22,6 +32,9 @@ function GluuTypeAhead({ label, name, value, options, formik, required }) {
           defaultSelected={value}
           options={options}
         />
+        <ThemeProvider theme={theme}>
+          <Typography variant="subtitle1">Enter multiple items by selecting from appeared dropdown after entering each item.</Typography>
+        </ThemeProvider>
       </Col>
     </FormGroup>
   )
