fede3d615e9880bbc8ce68e111d61847655efaca
Arnab Dutta <arnab.bdutta@gmail.com>
2021-05-14 11:45:27 +0530

    fix: fix menu duplication

--- plugins/PluginSagasResolver.js
+++ plugins/PluginSagasResolver.js
@@ -2,8 +2,9 @@ import plugins from '../plugins.config'
 
 
 //get all metadata path
-let pluginSagas = [];
+
 function process() {
+    let pluginSagas = [];
     plugins.map((item) => (item.metadataFile)).forEach(async (path) => {
         pluginSagas = [...pluginSagas, ...(require(`${path}`)).default.sagas]
     })
