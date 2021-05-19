f0a6c2f8b6e3f023a24faed456f107849d16e46f
Thomas Gasmyr <Thomas@gluu.org>
2021-05-19 10:45:17 +0100

    feat(admin-ui): auth server plugin layout #109

--- plugins/auth-server/components/Configuration/Keys/KeysPage.js
+++ plugins/auth-server/components/Configuration/Keys/KeysPage.js
@@ -1,7 +1,43 @@
 import React from 'react'
+import {
+  UncontrolledTabs,
+  NavItem,
+  Nav,
+  TabPane,
+  Card,
+  CardBody,
+} from '../../../../../app/components'
+import JwksPage from './Jwks/JwksPage'
 
 function KeysPage() {
-  return <div></div>
+  return (
+    <Card>
+      <CardBody>
+        <UncontrolledTabs initialActiveTabId="overview">
+          <Nav pills className="mb-4 flex-column flex-md-row mt-4 mt-lg-0">
+            <NavItem>
+              <UncontrolledTabs.NavLink tabId="jwkpanel">
+                JWK Keys
+              </UncontrolledTabs.NavLink>
+            </NavItem>
+            <NavItem>
+              <UncontrolledTabs.NavLink tabId="otherpanel">
+                Others Keys
+              </UncontrolledTabs.NavLink>
+            </NavItem>
+          </Nav>
+          <UncontrolledTabs.TabContent>
+            <TabPane tabId="jwkpanel">
+              <JwksPage />
+            </TabPane>
+            <TabPane tabId="otherpanel">
+              <div>Not implemented yet</div>
+            </TabPane>
+          </UncontrolledTabs.TabContent>
+        </UncontrolledTabs>
+      </CardBody>
+    </Card>
+  )
 }
 
 export default KeysPage
