a8e6e77d37775e449075f7ec4731b3f09323a137
Thomas Gasmyr <Thomas@gluu.org>
2021-06-04 00:32:39 +0100

    feat(admin-ui): implement audit log on auth server plugin #132

--- app/redux/api/base.js
+++ app/redux/api/base.js
@@ -20,6 +20,7 @@ export const getDefaultClient = (JansConfigApi) => {
 
 export const getClient = (JansConfigApi, r_token, r_issuer) => {
   console.log('--------1---getclient')
+  console.log('#############' + JansConfigApi)
   const defaultClient = JansConfigApi.ApiClient.instance
   defaultClient.timeout = 50000
   const jansauth = defaultClient.authentications['jans-auth']
@@ -36,6 +37,8 @@ export const getClient = (JansConfigApi, r_token, r_issuer) => {
   }
   defaultClient.defaultHeaders = headers
   console.log('--------2---getclient')
+  console.log('++++++++++++++++ jans ' + jansauth)
+  console.log('++++++++++++++++ rtoken ' + r_token)
   jansauth.accessToken = r_token
   console.log('--------3---getclient')
   return defaultClient
