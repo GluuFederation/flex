6e026745694a206a27190f5fee99221a114bb429
cool-dev <mike.dev.biz@gmail.com>
2021-06-10 11:38:10 -0300

    fix(admin-ui): convert Gluu components from class component into functional

--- app/routes/Apps/Gluu/GluuNameValuesProperty.js
+++ app/routes/Apps/Gluu/GluuNameValuesProperty.js
@@ -5,51 +5,36 @@ import GluuLabel from '../Gluu/GluuLabel'
 import Typography from '@material-ui/core/Typography'
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 
-class GluuNameValuesProperty extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      formik: this.props.formik || null,
-      name: this.props.name,
-      label1: this.props.label1 || 'Name',
-      name1: this.props.name1 || 'name',
-      placeholder1: this.props.placeholder1 || 'Enter name',
-      label2: this.props.label2 || 'Value',
-      name2: this.props.name2 || 'value',
-      placeholder2: this.props.placeholder2 || 'Enter value',
-      value: this.props.value || [],
-      inputId: this.props.inputId || null,
-      options: this.props.options || null,
-      validator: this.props.validator || null,
-      items: this.props.items || [],
-      //dataArr: [{name: "", value: []}]
-      dataArr: [{ name: '', domains: [] }],
-    }
-    this.setValues()
-  }
+function GluuNameValuesProperty ({formik, name, label1, name1, placeholder1, label2, name2, placeholder2, 
+  value, inputId, options, validator, items, dataArr}) {
 
-  theme = createMuiTheme({
+  const [dataArray, setDataArray] = useState(dataArr)
+  const theme = createMuiTheme({
     typography: {
       subtitle1: {
         fontSize: 12,
       },
     },
   })
+  setValues()
 
-  setValues() {
+  const addClick = () => {
+    setDataArray([...dataArray, { name: '', domains: [] }])
+  }
+  const setValues = () => {
     //console.log(' setValue - Entry value = '+JSON.stringify(this.state.value))
-    var dataArr2 = this.state.dataArr;
-    if (this.state.value != null && this.state.value.length) {
+    var dataArr2 = dataArray;
+    if (value != null && value.length) {
       //console.log(' setValue -  this.state.value.length = '+this.state.value.length)
-      if (this.state.value.length == 0) {
+      if (value.length == 0) {
         //dataArr = this.state.dataArr
         return;
       }
-      for (var i = 0; i < this.state.value.length; i++) {
-        var elm = this.state.value[i]
+      for (var i = 0; i < value.length; i++) {
+        var elm = value[i]
         var valueList = []
         var opts = []
-        valueList = this.state.value[i].domains
+        valueList = value[i].domains
         //console.log('**** valueList= '+valueList)
         if (valueList != null) {
           for (var j = 0; j < valueList.length; j++) {
@@ -59,54 +44,87 @@ class GluuNameValuesProperty extends React.Component {
           }
         }
         console.log(' opts = ' + opts);
-        dataArr2[i] = { name: this.state.value[i].name, domains: opts };
+        dataArr2[i] = { name: value[i].name, domains: opts };
       }//for
-      console.log('****Final this.state.dataArr2 = ' + JSON.stringify(this.state.dataArr2))
-      this.setState({
-        dataArr: dataArr2
-      })
+      console.log('****Final this.state.dataArr2 = ' + JSON.stringify(dataArr2))
+      setDataArray(dataArr2)
     }//this.state.value != null 
-    console.log('****Final this.state.dataArr = ' + JSON.stringify(this.state.dataArr))
+    console.log('****Final this.state.dataArr = ' + JSON.stringify(dataArray))
   }
 
-  addClick() {
-    this.setState((prevState) => ({
-      dataArr: [...prevState.dataArr, { name: '', domains: [] }],
-    }))
+  const handleChangeValue = (i, selected, name) => e => {
+    //console.log('Inside handleChangeValue- Entry -  i '+i+' , e = '+e+" , selected = "+selected+', name = '+name);
+    var dataLength = selected.length
+    var obj = JSON.stringify(selected[dataLength - 1])
+    var jsonObj = JSON.parse(obj)
+    var obj_text = jsonObj[name]
+
+    //Update array values
+    let newDataArr = [...dataArray]
+    var modEle = newDataArr[e]
+    modEle.domains.push(obj_text)
+    newDataArr[e] = { ...newDataArr[e], domains: modEle.domains }
+    setDataArray( newDataArr )
+    //console.log('Inside handleChangeValue- dataArr 2 -'+ JSON.stringify(dataArr));
+    formik.setFieldValue(name, newDataArr)
   }
 
-  createUI() {
-    return this.state.dataArr.map((party, index) => (
+  const handleChange = (i) => e => {
+    //console.log('Inside handleChange- Entry -  i '+i+' , e = '+e );
+    const { name, value } = e.target
+    let newDataArr = [...dataArray]
+    newDataArr[i] = { ...newDataArr[i], [name]: value }
+    setDataArray( newDataArr )
+    formik.setFieldValue(name, newDataArr)
+  }
+
+  const removeClick = (i) => {
+    let newDataArr = [...dataArray]
+    newDataArr.splice(i, 1)
+    setDataArray( newDataArr )
+  }
+
+  return (
+    <Row>
+      <GluuLabel label="Parties" size={9} />
+      &nbsp;
+      <input
+        type="button"
+        value="Add more"
+        onClick={addClick}
+      />
+      &nbsp;
+      {dataArray.map((party, index) => (
       <div key={index}>
         <FormGroup row>
-          <GluuLabel label={this.state.label1} />
+          <GluuLabel label={label1} />
           <Col sm={9}>
             <Input
-              placeholder={this.state.placeholder1}
-              id={this.state.name1}
-              name={this.state.name1}
+              placeholder={placeholder1}
+              id={name1}
+              name={name1}
               defaultValue={party.name}
-              onChange={this.handleChange.bind(this, index)}
+              onChange={handleChange(index)}
             />
           </Col>
-          <GluuLabel label={this.state.label2} />
+          <GluuLabel label={label2} />
           <Col sm={8}>
             <Typeahead
               allowNew
               emptyLabel=""
-              labelKey={this.state.name2}
-              placeholder={this.state.placeholder2}
-              onInputChange={this.handleInputChange}
+              labelKey={name2}
+              placeholder={placeholder2}
+              onInputChange={handleInputChange}
               onChange={(selected) => {
-                this.handleChangeValue(this, index, selected, this.state.name2)
+                handleChangeValue(index, selected, name2)
               }}
-              id={this.state.name2}
-              name={this.state.name2}
+              id={name2}
+              name={name2}
               multiple={true}
               defaultSelected={party.domains}
-              options={this.state.options}
+              options={options}
             />
-            <ThemeProvider theme={this.theme}>
+            <ThemeProvider theme={theme}>
               <Typography variant="subtitle1">
                 Enter multiple items by selecting from appeared dropdown after
                 entering each item.
@@ -117,64 +135,13 @@ class GluuNameValuesProperty extends React.Component {
           <input
             type="button"
             value="remove"
-            onClick={this.removeClick.bind(this, index)}
+            onClick={() => removeClick(index)}
           />
         </FormGroup>
       </div>
-    ))
-  }
-
-  handleInputChange(input, e) {
-    console.log('value', input)
-  }
-
-  handleChangeValue(i, e, selected, name) {
-    //console.log('Inside handleChangeValue- Entry -  i '+i+' , e = '+e+" , selected = "+selected+', name = '+name);
-    var dataLength = selected.length
-    var obj = JSON.stringify(selected[dataLength - 1])
-    var jsonObj = JSON.parse(obj)
-    var obj_text = jsonObj[name]
-
-    //Update array values
-    let dataArr = [...this.state.dataArr]
-    var modEle = dataArr[e]
-    modEle.domains.push(obj_text)
-    dataArr[e] = { ...dataArr[e], domains: modEle.domains }
-    this.setState({ dataArr })
-    //console.log('Inside handleChangeValue- dataArr 2 -'+ JSON.stringify(dataArr));
-    this.state.formik.setFieldValue(this.state.name, dataArr)
-  }
-
-  handleChange(i, e) {
-    //console.log('Inside handleChange- Entry -  i '+i+' , e = '+e );
-    const { name, value } = e.target
-    let dataArr = [...this.state.dataArr]
-    dataArr[i] = { ...dataArr[i], [name]: value }
-    this.setState({ dataArr })
-    this.state.formik.setFieldValue(this.state.name, dataArr)
-  }
-
-  removeClick(i) {
-    let dataArr = [...this.state.dataArr]
-    dataArr.splice(i, 1)
-    this.setState({ dataArr })
-  }
-
-  render() {
-    return (
-      <Row>
-        <GluuLabel label="Parties" size={9} />
-        &nbsp;
-        <input
-          type="button"
-          value="Add more"
-          onClick={this.addClick.bind(this)}
-        />
-        &nbsp;
-        {this.createUI()}
-      </Row>
-    )
-  }
+    ))}
+    </Row>
+  )
 }
 
 export default GluuNameValuesProperty
