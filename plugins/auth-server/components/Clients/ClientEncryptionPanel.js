b222ac204d793aee5f1e1b8e1d6fea86f3fd7412
Thomas Gasmyr <Thomas@gluu.org>
2021-10-01 19:55:06 +0100

    fix(admin-ui): provide tooltip for config properties #189

--- plugins/auth-server/components/Clients/ClientEncryptionPanel.js
+++ plugins/auth-server/components/Clients/ClientEncryptionPanel.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Col, Container, FormGroup, Input } from '../../../../app/components'
 import GluuLabel from '../../../../app/routes/Apps/Gluu/GluuLabel'
 import GluuInputRow from '../../../../app/routes/Apps/Gluu/GluuInputRow'
+import GluuSelectRow from '../../../../app/routes/Apps/Gluu/GluuSelectRow'
 import { useTranslation } from 'react-i18next'
 const DOC_CATEGORY = 'openid_client'
 
@@ -68,205 +69,147 @@ function ClientEncryptionPanel({ client, formik, oidcConfiguration }) {
         doc_category={DOC_CATEGORY}
       />
       <FormGroup row>
-        <GluuLabel label="fields.access_token_signing_alg" size={4} />
-        <Col sm={2}>
-          <Input
-            type="select"
+        <Col sm={6}>
+          <GluuSelectRow
+            label="fields.access_token_signing_alg"
+            formik={formik}
+            value={client.accessTokenSigningAlg}
+            values={accessTokenSigningAlg}
+            lsize={6}
+            rsize={6}
             name="accessTokenSigningAlg"
-            id="accessTokenSigningAlg"
-            defaultValue={client.accessTokenSigningAlg}
-            onChange={formik.handleChange}
-          >
-            <option></option>
-            {accessTokenSigningAlg.map((item, key) => (
-              <option value={item} key={key}>
-                {item}
-              </option>
-            ))}
-          </Input>
+            doc_category={DOC_CATEGORY}
+          />
         </Col>
-        <GluuLabel label="fields.id_token_encrypted_response_alg" size={4} />
-        <Col sm={2}>
-          <Input
-            type="select"
+        <Col sm={6}>
+          <GluuSelectRow
+            label="fields.id_token_encrypted_response_alg"
+            formik={formik}
+            lsize={6}
+            rsize={6}
+            value={client.idTokenEncryptedResponseAlg}
+            values={idTokenEncryptedResponseAlg}
             name="idTokenEncryptedResponseAlg"
-            id="idTokenEncryptedResponseAlg"
-            defaultValue={client.idTokenEncryptedResponseAlg}
-            onChange={formik.handleChange}
-          >
-            <option></option>
-            {idTokenEncryptedResponseAlg.map((item, key) => (
-              <option value={item} key={key}>
-                {item}
-              </option>
-            ))}
-          </Input>
+            doc_category={DOC_CATEGORY}
+          />
         </Col>
       </FormGroup>
       <FormGroup row>
-        <GluuLabel label="fields.id_token_signed_response_alg" size={4} />
-        <Col sm={2}>
-          <Input
-            type="select"
+        <Col sm={6}>
+          <GluuSelectRow
+            label="fields.id_token_signed_response_alg"
+            formik={formik}
+            value={client.accessTokenSigningAlg}
+            values={idTokenSignedResponseAlg}
+            lsize={6}
+            rsize={6}
             name="idTokenSignedResponseAlg"
-            id="idTokenSignedResponseAlg"
-            defaultValue={client.idTokenSignedResponseAlg}
-            onChange={formik.handleChange}
-          >
-            <option></option>
-            {idTokenSignedResponseAlg.map((item, key) => (
-              <option value={item} key={key}>
-                {item}
-              </option>
-            ))}
-          </Input>
+            doc_category={DOC_CATEGORY}
+          />
         </Col>
-        <GluuLabel label="fields.id_token_encrypted_response_enc" size={4} />
-        <Col sm={2}>
-          <Input
-            type="select"
+        <Col sm={6}>
+          <GluuSelectRow
+            label="fields.id_token_encrypted_response_enc"
+            formik={formik}
+            value={client.idTokenEncryptedResponseEnc}
+            values={idTokenEncryptedResponseEnc}
+            lsize={6}
+            rsize={6}
             name="idTokenEncryptedResponseEnc"
-            id="idTokenEncryptedResponseEnc"
-            defaultValue={client.idTokenEncryptedResponseEnc}
-            onChange={formik.handleChange}
-          >
-            <option></option>
-            {idTokenEncryptedResponseEnc.map((item, key) => (
-              <option value={item} key={key}>
-                {item}
-              </option>
-            ))}
-          </Input>
+            doc_category={DOC_CATEGORY}
+          />
         </Col>
       </FormGroup>
       <FormGroup row>
-        <GluuLabel label="fields.request_object_encryption_alg" size={4} />
-        <Col sm={2}>
-          <Input
-            type="select"
+        <Col sm={6}>
+          <GluuSelectRow
+            label="fields.request_object_encryption_alg"
+            formik={formik}
+            value={client.requestObjectEncryptionAlg}
+            values={requestObjectEncryptedResponseAlg}
+            lsize={6}
+            rsize={6}
             name="requestObjectEncryptionAlg"
-            id="requestObjectEncryptionAlg"
-            defaultValue={client.requestObjectEncryptionAlg}
-            onChange={formik.handleChange}
-          >
-            <option></option>
-            {requestObjectEncryptedResponseAlg.map((item, key) => (
-              <option value={item} key={key}>
-                {item}
-              </option>
-            ))}
-          </Input>
+            doc_category={DOC_CATEGORY}
+          />
         </Col>
-        <GluuLabel label="fields.request_object_signing_alg" size={4} />
-        <Col sm={2}>
-          <Input
-            type="select"
+        <Col sm={6}>
+          <GluuSelectRow
+            label="fields.request_object_signing_alg"
+            formik={formik}
+            value={client.requestObjectSigningAlg}
+            values={requestObjectSignedResponseAlg}
+            lsize={6}
+            rsize={6}
             name="requestObjectSigningAlg"
-            id="requestObjectSigningAlg"
-            defaultValue={client.requestObjectSigningAlg}
-            onChange={formik.handleChange}
-          >
-            <option></option>
-            {requestObjectSignedResponseAlg.map((item, key) => (
-              <option value={item} key={key}>
-                {item}
-              </option>
-            ))}
-          </Input>
+            doc_category={DOC_CATEGORY}
+          />
         </Col>
       </FormGroup>
       <FormGroup row>
-        <GluuLabel label="fields.request_object_encryption_enc" size={4} />
-        <Col sm={2}>
-          <Input
-            type="select"
+        <Col sm={6}>
+          <GluuSelectRow
+            label="fields.request_object_encryption_enc"
+            formik={formik}
+            value={client.requestObjectEncryptionEnc}
+            values={requestObjectEncryptedResponseEnc}
+            lsize={6}
+            rsize={6}
             name="requestObjectEncryptionEnc"
-            id="requestObjectEncryptionEnc"
-            defaultValue={client.requestObjectEncryptionEnc}
-            onChange={formik.handleChange}
-          >
-            <option></option>
-            {requestObjectEncryptedResponseEnc.map((item, key) => (
-              <option value={item} key={key}>
-                {item}
-              </option>
-            ))}
-          </Input>
+            doc_category={DOC_CATEGORY}
+          />
         </Col>
-        <GluuLabel label="fields.token_endpoint_auth_method" size={4} />
-        <Col sm={2}>
-          <Input
-            type="select"
+        <Col sm={6}>
+          <GluuSelectRow
+            label="fields.token_endpoint_auth_method"
+            formik={formik}
+            value={client.tokenEndpointAuthMethod}
+            values={tokenEndpointAuthMethod}
+            lsize={6}
+            rsize={6}
             name="tokenEndpointAuthMethod"
-            id="tokenEndpointAuthMethod"
-            defaultValue={client.tokenEndpointAuthMethod}
-            onChange={formik.handleChange}
-          >
-            <option></option>
-            {tokenEndpointAuthMethod.map((item, key) => (
-              <option value={item} key={key}>
-                {item}
-              </option>
-            ))}
-          </Input>
+            doc_category={DOC_CATEGORY}
+          />
         </Col>
       </FormGroup>
 
       <FormGroup row>
-        <GluuLabel label="fields.user_info_encrypted_response_alg" size={4} />
-        <Col sm={2}>
-          <Input
-            type="select"
+        <Col sm={6}>
+          <GluuSelectRow
+            label="fields.user_info_encrypted_response_alg"
+            formik={formik}
+            value={client.userInfoEncryptedResponseAlg}
+            values={userInfoEncryptedResponseAlg}
+            lsize={6}
+            rsize={6}
             name="userInfoEncryptedResponseAlg"
-            id="userInfoEncryptedResponseAlg"
-            defaultValue={client.userInfoEncryptedResponseAlg}
-            onChange={formik.handleChange}
-          >
-            <option></option>
-            {userInfoEncryptedResponseAlg.map((item, key) => (
-              <option value={item} key={key}>
-                {item}
-              </option>
-            ))}
-          </Input>
-        </Col>
-        <GluuLabel label="fields.user_info_signed_response_alg" size={4} />
-        <Col sm={2}>
-          <Input
-            type="select"
+            doc_category={DOC_CATEGORY}
+          />
+        </Col>{' '}
+        <Col sm={6}>
+          <GluuSelectRow
+            label="fields.user_info_signed_response_alg"
+            formik={formik}
+            value={client.userInfoSignedResponseAlg}
+            values={userInfoSignedResponseAlg}
+            lsize={6}
+            rsize={6}
             name="userInfoSignedResponseAlg"
-            id="userInfoSignedResponseAlg"
-            defaultValue={client.userInfoSignedResponseAlg}
-            onChange={formik.handleChange}
-          >
-            <option></option>
-            {userInfoSignedResponseAlg.map((item, key) => (
-              <option value={item} key={key}>
-                {item}
-              </option>
-            ))}
-          </Input>
+            doc_category={DOC_CATEGORY}
+          />
         </Col>
       </FormGroup>
-      <FormGroup row>
-        <GluuLabel label="fields.user_info_encrypted_response_enc" size={8} />
-        <Col sm={4}>
-          <Input
-            type="select"
-            name="userInfoEncryptedResponseEnc"
-            id="userInfoEncryptedResponseEnc"
-            defaultValue={client.userInfoEncryptedResponseEnc}
-            onChange={formik.handleChange}
-          >
-            <option></option>
-            {userInfoEncryptedResponseEnc.map((item, key) => (
-              <option value={item} key={key}>
-                {item}
-              </option>
-            ))}
-          </Input>
-        </Col>
-      </FormGroup>
+
+      <GluuSelectRow
+        label="fields.user_info_encrypted_response_enc"
+        formik={formik}
+        value={client.userInfoEncryptedResponseEnc}
+        values={userInfoEncryptedResponseEnc}
+        lsize={6}
+        rsize={6}
+        name="userInfoEncryptedResponseEnc"
+        doc_category={DOC_CATEGORY}
+      />
     </Container>
   )
 }
