7fd1332c53aae41f62ae0d3547923273dc45558d
Thomas Gasmyr <Thomas@gluu.org>
2021-10-06 21:20:20 +0100

    fix(admin-ui): add tooltip for Add/Edit form input fields #269

--- plugins/admin/components/Settings/SettingsPage.js
+++ plugins/admin/components/Settings/SettingsPage.js
@@ -1,71 +1,108 @@
-import React, { useEffect, useState } from 'react'
-import { useTranslation } from "react-i18next";
+import React, { useState } from 'react'
+import { useTranslation } from 'react-i18next'
 import { connect } from 'react-redux'
-import { Formik } from 'formik'
 
 import GluuLabel from '../../../../app/routes/Apps/Gluu/GluuLabel'
-import GluuSelectRow from '../../../../app/routes/Apps/Gluu/GluuSelectRow';
+import GluuTooltip from '../../../../app/routes/Apps/Gluu/GluuTooltip'
+import { SETTINGS } from '../../../../app/utils/ApiResources'
 
-import { Form, Container, Card, CardBody, FormGroup, Col, Label, Badge, InputGroup, CustomInput } from '../../../../app/components'
-import GluuDarkModeToggle from '../../../../app/routes/Apps/Gluu/GluuDarkModeToggle';
+import {
+  Card,
+  CardBody,
+  FormGroup,
+  Col,
+  Label,
+  Badge,
+  InputGroup,
+  CustomInput,
+} from '../../../../app/components'
+import GluuDarkModeToggle from '../../../../app/routes/Apps/Gluu/GluuDarkModeToggle'
 
-function SettingsPage({  }) {
-  const { t } = useTranslation();
-  const [paggingSize, setPaggingSize] = useState(localStorage.getItem('paggingSize') || 10)
-  
+function SettingsPage({}) {
+  const { t } = useTranslation()
+  const [paggingSize, setPaggingSize] = useState(
+    localStorage.getItem('paggingSize') || 10,
+  )
   const levels = [1, 5, 10, 20]
   return (
     <React.Fragment>
-        {/* <Container> */}
-          <Card>
-            <CardBody>
-              <FormGroup row>
-                <GluuLabel label="List paging size" size={4} />
-                <Col sm={8}>
-                  <InputGroup>
-                    <CustomInput
-                      type="select"
-                      id="pagingSize"
-                      name="pagingSize"
-                      defaultValue={levels[levels.findIndex((element) => { return element == paggingSize })]}
-                      onChange={(value) => {
-                        let size = levels[value.target.options.selectedIndex]
-                        setPaggingSize(size)
-                        localStorage.setItem('paggingSize', size)
-                      }}
-                    >
-                      {/* <option value="">{t('actions.choose')}...</option> */}
-                      {levels.map((item, key) => (
-                        <option value={item} key={key}>
-                          {item}
-                        </option>
-                      ))}
-                    </CustomInput>
-                  </InputGroup>
-                </Col>
-              </FormGroup>
-              <FormGroup row style={{justifyContent:"space-between"}}>
-                <GluuLabel label="Dark Mode" />
-                <div className="toggle-container" style={{display:"flex", flexDirection:"column", justifyContent:"center", padding:"0px 15px 0px 15px"}}>
-                  <GluuDarkModeToggle />
-                </div>
-              </FormGroup>
-              <FormGroup row style={{justifyContent:"space-between"}}>
-                <GluuLabel label="Config API URL" />
-                <Label style={{display:"flex", flexDirection:"column", justifyContent:"center", paddingLeft:"15px", paddingRight:"15px"}} >
-                  <h3><Badge color={'primary'} >{process.env.CONFIG_API_BASE_URL}</Badge></h3>
-                </Label>
-              </FormGroup>
-            </CardBody>
-          </Card>
-        {/* </Container> */}
+      <Card>
+        <CardBody>
+          <GluuTooltip doc_category={SETTINGS} doc_entry="pageSize">
+            <FormGroup row>
+              <GluuLabel label="List paging size" size={4} />
+              <Col sm={8}>
+                <InputGroup>
+                  <CustomInput
+                    type="select"
+                    id="pagingSize"
+                    name="pagingSize"
+                    defaultValue={
+                      levels[
+                        levels.findIndex((element) => {
+                          return element == paggingSize
+                        })
+                      ]
+                    }
+                    onChange={(value) => {
+                      let size = levels[value.target.options.selectedIndex]
+                      setPaggingSize(size)
+                      localStorage.setItem('paggingSize', size)
+                    }}
+                  >
+                    {levels.map((item, key) => (
+                      <option value={item} key={key}>
+                        {item}
+                      </option>
+                    ))}
+                  </CustomInput>
+                </InputGroup>
+              </Col>
+            </FormGroup>
+          </GluuTooltip>
+          <GluuTooltip doc_category={SETTINGS} doc_entry="darkMode">
+            <FormGroup row style={{ justifyContent: 'space-between' }}>
+              <GluuLabel label="Dark Mode" />
+              <div
+                className="toggle-container"
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  padding: '0px 15px 0px 15px',
+                }}
+              >
+                <GluuDarkModeToggle />
+              </div>
+            </FormGroup>
+          </GluuTooltip>
+          <GluuTooltip doc_category={SETTINGS} doc_entry="configApiUrl">
+            <FormGroup row style={{ justifyContent: 'space-between' }}>
+              <GluuLabel label="Config API URL" />
+              <Label
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  paddingLeft: '15px',
+                  paddingRight: '15px',
+                }}
+              >
+                <h3>
+                  <Badge color={'primary'}>
+                    {process.env.CONFIG_API_BASE_URL}
+                  </Badge>
+                </h3>
+              </Label>
+            </FormGroup>
+          </GluuTooltip>
+        </CardBody>
+      </Card>
     </React.Fragment>
   )
 }
 
 const mapStateToProps = (state) => {
-  return {
-
-  }
+  return {}
 }
 export default connect(mapStateToProps)(SettingsPage)
