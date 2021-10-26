53d441ba1393218d5def23945ea22fc94a1fdbb0
Thomas Gasmyr <Thomas@gluu.org>
2021-10-26 07:36:52 +0100

    fix(admin-ui): sample testing

--- plugins/auth-server/components/Clients/ClientListPage.test.js
+++ plugins/auth-server/components/Clients/ClientListPage.test.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import renderer from 'react-test-renderer'
 import { Provider } from 'react-redux'
 import { createStore } from "redux";
 import ClientListPage from './ClientListPage'
