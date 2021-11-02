144dabf5a32eca3cc5c6cf56728bb903065e3327
Thomas Gasmyr <Thomas@gluu.org>
2021-11-02 20:33:19 +0100

    fix(admin-ui): write test for all Gluu custom base UI components #290

--- app/routes/Apps/Gluu/Tests/GluuSecretDetail.test.js
+++ app/routes/Apps/Gluu/Tests/GluuSecretDetail.test.js
@@ -1,29 +1,24 @@
 import React from 'react'
-import GluuTypeAheadWithAdd from '../GluuTypeAheadWithAdd'
-import { render, screen, fireEvent } from '@testing-library/react'
+import GluuSecretDetail from '../GluuSecretDetail'
+import { render, screen } from '@testing-library/react'
 import i18n from '../../../../i18n'
 import { I18nextProvider } from 'react-i18next'
 
-it('Test GluuTypeAheadWithAdd component', () => {
+it('Test GluuSecretDetail component', () => {
   const LABEL = 'fields.application_type'
-  const NAME = 'applicationType'
-  const VALUE = ['Monday']
-  const OPTIONS = ['Monday', 'Tuesday']
+  const VALUE = 'computer'
   render(
     <I18nextProvider i18n={i18n}>
-      <GluuTypeAheadWithAdd
+      <GluuSecretDetail
         doc_category="openid_client"
-        name={NAME}
+        doc_entry="applicationType"
         value={VALUE}
+        up
         label={LABEL}
-        options={OPTIONS}
       />
     </I18nextProvider>,
   )
-  screen.getByText('Application Type:')
-  screen.getByText('Add')
-  screen.getByText('Remove')
-  screen.getByText(VALUE[0])
+  screen.getByText(/Application Type:/)
   screen.getByText('The OpenID connect Client application type.')
   expect(
     screen.getByText('The OpenID connect Client application type.'),
