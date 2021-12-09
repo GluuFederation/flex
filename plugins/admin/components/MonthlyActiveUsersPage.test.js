c1ec8bfda6cb19a3178a1355afbff1dbe9e5d182
Fausto <faustoalquiler@gmail.com>
2021-12-08 18:21:38 -0800

    fix: code smells

--- plugins/admin/components/MonthlyActiveUsersPage.test.js
+++ plugins/admin/components/MonthlyActiveUsersPage.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import MonthlyActiveUsersPage from './MonthlyActiveUsersPage'
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
--- plugins/admin/components/Roles/UiRoleListPage.test.js
+++ plugins/admin/components/Roles/UiRoleListPage.test.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import UiRoleListPage from './UiRoleListPage'
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
