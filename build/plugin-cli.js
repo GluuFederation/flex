d3eec047036f4aea9289f99a28a25a2a074ab5f6
Arnab Dutta <arnab.bdutta@gmail.com>
2022-01-10 13:19:18 +0530

    create cli tool for adding/removing plugins #351

--- build/plugin-cli.js
+++ build/plugin-cli.js
@@ -44,15 +44,23 @@ const commands = {
         if(ele.enabled)
         fse.copySync(path.join(pluginsRepoPath, ele.key), path.join(pluginsPath, ele.key))
       });
-      console.info('Added enabled plugins successfully. The project is ready for built.');
+      console.info('Added/Removed enabled plugins successfully. The project is ready for built.');
     },
     showEnabledPlugins: function() {
+      if(pluginsObj.filter(ele => ele.enabled).length == 0) {
+        console.log('- No Plugin enabled.')
+        return;
+      }
       pluginsObj.forEach(ele => {
         if(ele.enabled)
           console.log('- '+ele.key)
       });
     },
     showAllPlugins: function() {
+      if(pluginsObj.length == 0) {
+        console.log('- No Plugin in repo.')
+        return;
+      }
       console.log('Following plugins are present in plugin repository.')
       pluginsObj.forEach(ele => {
       console.log('- '+ele.key)
@@ -60,7 +68,7 @@ const commands = {
     },
     enablePlugins: function(value) {
       this.createPluginRepo();
-      console.log('Following plugins are enabled.')
+      console.info('Following plugins are enabled.')
       this.showEnabledPlugins();
       var pluginsArr = value.split('_,');
       try {
@@ -76,12 +84,12 @@ const commands = {
       } catch (e) {
         console.error('Error in enabling plugin. Check the plugin keys entered in args.')
       }
-      console.log('Following plugins are enabled now.')
+      console.info('Following plugins are enabled now.')
       this.showEnabledPlugins();
     },
     disablePlugins: function(value) {
       this.createPluginRepo();
-      console.log('Following plugins are enabled.')
+      console.info('Following plugins are enabled.')
       this.showEnabledPlugins();
       var pluginsArr = value.split('_,');
       try {
@@ -89,26 +97,52 @@ const commands = {
           if (pluginsArr.indexOf(ele.key) >= 0) {
             ele.enabled = false;
           }
-    
           if (index == (pluginsObj.length-1)) {
-            fse.writeFileSync(path.join(dirParamToPath('rootDir'), 'plugins.config.json'),JSON.stringify(pluginsObj, null, 2)) 
-            console.log('Following plugins are enabled now.')
-            this.addEnabledPlugins();
+              fse.writeFileSync(path.join(dirParamToPath('rootDir'), 'plugins.config.json'),JSON.stringify(pluginsObj, null, 2)) 
+              this.addEnabledPlugins();
           }
         });
       } catch (e) {
         console.error('Error in enabling plugin. Check the plugin keys entered in args.')
       }
+      console.info('Following plugins are enabled now.')
       this.showEnabledPlugins();
     },
+    installPlugin: function(sourcePath) {
+      const pluginsRepoPath = dirParamToPath('plugins_repo');
 
+      this.createPluginRepo();
+      console.info('Following plugins are enabled.')
+      this.showEnabledPlugins();
+      if (!fse.existsSync(sourcePath)) {
+        return;
+      }
+      var lastFolder = sourcePath.split('/').filter(function(el) {
+        return el.trim().length > 0;
+      }).pop();
+
+      fse.copySync(sourcePath, path.join(pluginsRepoPath, lastFolder))
+
+      pluginsObj.push(this.createPluginEntry(lastFolder));
+      fse.writeFileSync(path.join(dirParamToPath('rootDir'), 'plugins.config.json'),JSON.stringify(pluginsObj, null, 2)) 
+
+    },
+    createPluginEntry: function(pluginKey) {
+      const pluginObj = {};
+      pluginObj.key = pluginKey;
+      pluginObj.metadataFile = './' + path.join(pluginKey, 'plugin-metadata');
+      pluginObj.enabled = false;
+
+      return pluginObj;
+    }
 };
 
 program
     .option('-sep, --showEnabledPlugins')
     .option('-sap, --showAllPlugins')
-    .option('-sap, --enablePlugins []')
-    .option('-sap, --disablePlugins []')
+    .option('-ep, --enablePlugins []')
+    .option('-dp, --disablePlugins []')
+    .option('-ip, --installPlugin []')
     .parse(process.argv);
 
 for (const commandName in commands) {
