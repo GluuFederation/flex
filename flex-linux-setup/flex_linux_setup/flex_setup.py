116b791b80544ac9fe9fd4266f662b18397bea19
Mustafa Baser <mbaser@mail.com>
2022-02-25 23:49:09 +0300

    fix: flex-linux-setup pass -yes to install.py

--- flex-linux-setup/flex_linux_setup/flex_setup.py
+++ flex-linux-setup/flex_linux_setup/flex_setup.py
@@ -33,6 +33,8 @@ except ModuleNotFoundError:
         install_url = 'https://raw.githubusercontent.com/JanssenProject/jans/{}/jans-linux-setup/jans_setup/install.py'.format(setup_branch)
         request.urlretrieve(install_url, 'install.py')
         install_cmd = 'python3 install.py --setup-branch={} --no-setup'.format(setup_branch)
+        if '-yes' in sys.argv:
+            install_cmd += ' -yes'
         print("Executing", install_cmd)
         os.system(install_cmd)
         sys.path.append(__STATIC_SETUP_DIR__)
