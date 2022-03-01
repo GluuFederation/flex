37d68d2b6d4a6f99775581e247259ed6a84c7016
iromli <isman.firmansyah@gmail.com>
2022-03-02 03:12:03 +0700

    fix: download plugins with dependencies

--- docker-casa/scripts/entrypoint.sh
+++ docker-casa/scripts/entrypoint.sh
@@ -2,14 +2,14 @@
 set -e
 
 get_casa_plugins(){
-  # ===============
-  # PREPARE PLUGINS
-  # ===============
-  CASA_PLUGIN_DIRECTORY=/opt/jans/jetty/casa/plugins
-  CASA_PLUGIN_REPO=https://maven.gluu.org/maven/org/gluu/casa/plugins
-  wget -q ${CASA_PLUGIN_REPO}/strong-authn-settings/${GLUU_VERSION}/strong-authn-settings-${GLUU_VERSION}.jar -O ${CASA_PLUGIN_DIRECTORY}/strong-authn-settings-${GLUU_VERSION}.jar \
-    && wget -q ${CASA_PLUGIN_REPO}/authorized-clients/${GLUU_VERSION}/authorized-clients-${GLUU_VERSION}.jar -O ${CASA_PLUGIN_DIRECTORY}/authorized-clients-${GLUU_VERSION}.jar \
-    && wget -q ${CASA_PLUGIN_REPO}/custom-branding/${GLUU_VERSION}/custom-branding-${GLUU_VERSION}.jar -O ${CASA_PLUGIN_DIRECTORY}/custom-branding-${GLUU_VERSION}.jar
+    # ===============
+    # PREPARE PLUGINS
+    # ===============
+    CASA_PLUGIN_DIRECTORY=/opt/jans/jetty/casa/plugins
+    CASA_PLUGIN_REPO=https://maven.gluu.org/maven/org/gluu/casa/plugins
+    wget -q ${CASA_PLUGIN_REPO}/strong-authn-settings/${GLUU_VERSION}/strong-authn-settings-${GLUU_VERSION}-jar-with-dependencies.jar -O ${CASA_PLUGIN_DIRECTORY}/strong-authn-settings-${GLUU_VERSION}.jar \
+    && wget -q ${CASA_PLUGIN_REPO}/authorized-clients/${GLUU_VERSION}/authorized-clients-${GLUU_VERSION}-jar-with-dependencies.jar -O ${CASA_PLUGIN_DIRECTORY}/authorized-clients-${GLUU_VERSION}.jar \
+    && wget -q ${CASA_PLUGIN_REPO}/custom-branding/${GLUU_VERSION}/custom-branding-${GLUU_VERSION}-jar-with-dependencies.jar -O ${CASA_PLUGIN_DIRECTORY}/custom-branding-${GLUU_VERSION}.jar
     #&& wget -q ${CASA_PLUGIN_REPO}/account-linking/${GLUU_VERSION}/account-linking-${GLUU_VERSION}.jar -O ${CASA_PLUGIN_DIRECTORY}/account-linking-${GLUU_VERSION}.jar \
     #&& wget -q ${CASA_PLUGIN_REPO}/bioid-plugin/${GLUU_VERSION}/bioid-plugin-${GLUU_VERSION}.jar -O ${CASA_PLUGIN_DIRECTORY}/bioid-plugin-${GLUU_VERSION}.jar \
     #&& wget -q ${CASA_PLUGIN_REPO}/cert-authn/${GLUU_VERSION}/cert-authn-${GLUU_VERSION}.jar -O ${CASA_PLUGIN_DIRECTORY}/cert-authn-${GLUU_VERSION}.jar \
@@ -29,7 +29,8 @@ python3 /app/scripts/bootstrap.py
 # python3 /app/scripts/jca_sync.py &
 python3 /app/scripts/auth_conf.py
 
-get_casa_plugins || echo "Casa plugins were not downloaded. You may mount the plugins at /opt/jans/jetty/casa/plugins. For more information please go to https://gluu.org/docs/casa/latest/plugins/2fa-settings/"
+get_casa_plugins
+
 # run Casa server
 cd /opt/jans/jetty/casa
 exec java \
