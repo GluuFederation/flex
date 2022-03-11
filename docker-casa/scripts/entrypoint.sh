7d0e928fc38125579a2aad2a11b6073dfec3c9f6
iromli <isman.firmansyah@gmail.com>
2022-03-11 16:35:02 +0700

    refactor: set arguments for jetty via command-line
    - jetty.httpConfig.sendServerVersion=false to disable server info
    - jetty.deploy.scanInterval=0 to disable hot-deployment

--- docker-casa/scripts/entrypoint.sh
+++ docker-casa/scripts/entrypoint.sh
@@ -45,4 +45,4 @@ exec java \
     -Djava.io.tmpdir=/opt/jetty/temp \
     -Dlog4j2.configurationFile=resources/log4j2.xml \
     ${CN_JAVA_OPTIONS} \
-    -jar /opt/jetty/start.jar
+    -jar /opt/jetty/start.jar jetty.httpConfig.sendServerVersion=false jetty.deploy.scanInterval=0
