a1e621d5f3578bbcac93755aa6705a68429e04d3
Isman Firmansyah <iromli@users.noreply.github.com>
2022-05-01 14:39:24 +0700

    feat: casa updated dependencies and manifests (#202)

--- docker-casa/scripts/bootstrap.py
+++ docker-casa/scripts/bootstrap.py
@@ -1,8 +1,11 @@
+import contextlib
 import json
 import logging.config
 import os
 import re
+from uuid import uuid4
 from string import Template
+from functools import cached_property
 
 from jans.pycloudlib import get_manager
 from jans.pycloudlib.persistence import render_couchbase_properties
@@ -14,7 +17,16 @@ from jans.pycloudlib.persistence import sync_couchbase_truststore
 from jans.pycloudlib.persistence import sync_ldap_truststore
 from jans.pycloudlib.persistence import render_sql_properties
 from jans.pycloudlib.persistence import render_spanner_properties
+from jans.pycloudlib.persistence import CouchbaseClient
+from jans.pycloudlib.persistence import LdapClient
+from jans.pycloudlib.persistence import SpannerClient
+from jans.pycloudlib.persistence import SqlClient
+from jans.pycloudlib.persistence import doc_id_from_dn
+from jans.pycloudlib.persistence import id_from_dn
 from jans.pycloudlib.utils import cert_to_truststore
+from jans.pycloudlib.utils import get_random_chars
+from jans.pycloudlib.utils import encode_text
+from jans.pycloudlib.utils import generate_base64_contents
 
 from settings import LOGGING_CONFIG
 
@@ -175,6 +187,113 @@ def main():
     modify_jetty_xml()
     modify_webdefault_xml()
     configure_logging()
+
+    persistence_setup = PersistenceSetup(manager)
+    persistence_setup.import_ldif_files()
+
+
+class PersistenceSetup:
+    def __init__(self, manager):
+        self.manager = manager
+
+        client_classes = {
+            "ldap": LdapClient,
+            "couchbase": CouchbaseClient,
+            "spanner": SpannerClient,
+            "sql": SqlClient,
+        }
+
+        # determine persistence type
+        self.persistence_type = os.environ.get("CN_PERSISTENCE_TYPE", "ldap")
+        ldap_mapping = os.environ.get("CN_PERSISTENCE_LDAP_MAPPING", "default")
+
+        if self.persistence_type == "hybrid":
+            if ldap_mapping == "default":
+                client_cls = LdapClient
+                self.persistence_type = "ldap"
+            else:
+                client_cls = CouchbaseClient
+                self.persistence_type = "couchbase"
+
+        # determine persistence client
+        client_cls = client_classes.get(self.persistence_type)
+        self.client = client_cls(manager)
+
+    @cached_property
+    def ctx(self):
+        hostname = self.manager.config.get("hostname")
+
+        ctx = {
+            "hostname": hostname,
+            "casa_redirect_uri": f"https://{hostname}/casa",
+            "casa_redirect_logout_uri": f"https://{hostname}/casa/bye.zul",
+            "casa_frontchannel_logout_uri": f"https://{hostname}/casa/autologout",
+        }
+
+        with open("/app/static/extension/person_authentication/Casa.py") as f:
+            ctx["casa_person_authentication_script"] = generate_base64_contents(f.read())
+
+        # Casa client
+        ctx["casa_client_id"] = self.manager.config.get("casa_client_id")
+        if not ctx["casa_client_id"]:
+            ctx["casa_client_id"] = f"1902.{uuid4()}"
+            self.manager.config.set("casa_client_id", ctx["casa_client_id"])
+
+        ctx["casa_client_pw"] = self.manager.secret.get("casa_client_pw")
+        if not ctx["casa_client_pw"]:
+            ctx["casa_client_pw"] = get_random_chars()
+            self.manager.secret.set("casa_client_pw", ctx["casa_client_pw"])
+
+        ctx["casa_client_encoded_pw"] = self.manager.secret.get("casa_client_encoded_pw")
+        if not ctx["casa_client_encoded_pw"]:
+            ctx["casa_client_encoded_pw"] = encode_text(
+                ctx["casa_client_pw"], self.manager.secret.get("encoded_salt"),
+            ).decode()
+            self.manager.secret.set("casa_client_encoded_pw", ctx["casa_client_encoded_pw"])
+
+        # finalized contexts
+        return ctx
+
+    @cached_property
+    def ldif_files(self):
+        filenames = ["casa_config.ldif", "casa_client.ldif"]
+        # add casa_person_authentication_script.ldif if there's no existing casa script in persistence to avoid error
+        # java.lang.IllegalStateException: Duplicate key casa (attempted merging values 1 and 1)
+        if not self._deprecated_script_exists():
+            filenames.append("casa_person_authentication_script.ldif")
+        return [f"/app/templates/{filename}" for filename in filenames]
+
+    def _deprecated_script_exists(self):
+        script_exists = False
+
+        # deprecated Casa script DN
+        id_ = "inum=BABA-CACA,ou=scripts,o=jans"
+
+        # sql and spanner
+        if self.persistence_type in ("sql", "spanner"):
+            script_exists = bool(self.client.get("jansCustomScr", doc_id_from_dn(id_)))
+
+        # couchbase
+        elif self.persistence_type == "couchbase":
+            bucket = os.environ.get("CN_COUCHBASE_BUCKET_PREFIX", "jans")
+            key = id_from_dn(id_)
+            req = self.client.exec_query(
+                f"SELECT META().id, {bucket}.* FROM {bucket} USE KEYS '{key}'"
+            )
+            with contextlib.suppress(IndexError):
+                entry = req.json()["results"][0]
+                script_exists = bool(entry["id"])
+
+        # ldap
+        else:
+            script_exists = bool(self.client.get(id_))
+
+        return script_exists
+
+    def import_ldif_files(self):
+        for file_ in self.ldif_files:
+            logger.info(f"Importing {file_}")
+            self.client.create_from_ldif(file_, self.ctx)
 
 
 if __name__ == "__main__":
