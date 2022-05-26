5fe8fe194a6dcf88894245b2cbfe4b057e0ceb7a
= <mjatin78@gmail.com>
2022-05-26 14:11:33 +0530

    feat(admin-ui): show list of users

--- admin-ui/plugins/admin/components/UserManagement/UserDetailViewPage.js
+++ admin-ui/plugins/admin/components/UserManagement/UserDetailViewPage.js
@@ -3,9 +3,7 @@ import { Container, Row, Col } from '../../../../app/components'
 import GluuFormDetailRow from '../../../../app/routes/Apps/Gluu/GluuFormDetailRow'
 const UserDetailViewPage = ({ row }) => {
   const { rowData } = row
-  useEffect(() => {
-    console.log(row)
-  }, [row])
+
   return (
     <React.Fragment>
       <Container style={{ backgroundColor: '#F5F5F5' }}>
@@ -25,9 +23,16 @@ const UserDetailViewPage = ({ row }) => {
           <Col sm={4}>
             <GluuFormDetailRow label="fields.userName" value={rowData.userId} />
           </Col>
-          <Col sm={6}>
+          <Col sm={4}>
             <GluuFormDetailRow label="fields.email" value={rowData?.mail} />
           </Col>
+          {rowData.customAttributes?.map((data, key) => {
+            return (
+              <Col sm={4} key={'customAttributes' + key}>
+                <GluuFormDetailRow label={data.name} value={data?.values[0]} />
+              </Col>
+            )
+          })}
         </Row>
       </Container>
     </React.Fragment>
