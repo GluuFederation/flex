0da47c8973c4f38842f6271764729da10574196e
Harry Andriyan <harry.andriyan@gmail.com>
2022-06-07 22:01:06 +0700

    feat(redux store): update deprecated createStore

--- admin-ui/app/components/App/AppMain.js
+++ admin-ui/app/components/App/AppMain.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { configureStore } from 'Redux/store'
+import { configStore } from 'Redux/store'
 import { PersistGate } from 'redux-persist/integration/react'
 import AuthenticatedRouteSelector from './AuthenticatedRouteSelector'
 const basePath = process.env.BASE_PATH || '/admin'
 
 const AppMain = () => {
-  const { store, persistor } = configureStore()
+  const { store, persistor } = configStore()
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
