aa9590615faaa3b2ba11e3477dbf89a07ba09d1e
Harry Andriyan <harry.andriyan@gmail.com>
2022-06-09 17:22:31 +0700

    feat(husky): test

--- admin-ui/app/index.js
+++ admin-ui/app/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from 'Components/App'
 import { I18nextProvider } from 'react-i18next'
-import i18n from './i18n'
+import i18n from './i18n';
 
 render(
   <I18nextProvider i18n={i18n}>
