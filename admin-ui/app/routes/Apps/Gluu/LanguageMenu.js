35803821caa08b5e4d28221db9a815a13d8053b6
Harry Andriyan <harry.andriyan@gmail.com>
2022-08-22 10:00:58 +0700

    feat(admin-ui): remember languange and theme config after user logout

--- admin-ui/app/routes/Apps/Gluu/LanguageMenu.js
+++ admin-ui/app/routes/Apps/Gluu/LanguageMenu.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import {
   DropdownToggle,
@@ -9,13 +9,23 @@ import {
 
 const LanguageMenu = () => {
   const [isOpen, setOpen] = useState(false)
+  const initLang = localStorage.getItem('initLang')
   const [lang, setLang] = useState('en')
   const { t, i18n } = useTranslation()
   const toggle = () => setOpen(!isOpen)
+
   function changeLanguage(code) {
     i18n.changeLanguage(code)
     setLang(code)
+    localStorage.setItem('initLang', code)
   }
+
+  useEffect(() => {
+    i18n.changeLanguage(initLang)
+    const currentLang = initLang ? initLang : 'en'
+    setLang(currentLang)
+  }, [initLang])
+
   return (
     <ButtonDropdown isOpen={isOpen} toggle={toggle}>
       <DropdownToggle caret color="transparent" data-testid="ACTIVE_LANG">
