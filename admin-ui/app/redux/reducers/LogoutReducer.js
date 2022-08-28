d6ca354c2caec3fd646c724aae2d83541f849b29
Harry Andriyan <harry.andriyan@gmail.com>
2022-08-28 11:21:50 +0700

    feat(admin-ui): user lang and theme config for each user

--- admin-ui/app/redux/reducers/LogoutReducer.js
+++ admin-ui/app/redux/reducers/LogoutReducer.js
@@ -9,9 +9,11 @@ export default function logoutReducer(state = INIT_STATE, action) {
   if (action.type === USER_LOGGED_OUT) {
     const initTheme = localStorage.getItem('initTheme')
     const initLang = localStorage.getItem('initLang')
+    const userConfig = localStorage.getItem('userConfig')
     localStorage.clear()
     localStorage.setItem('initTheme', initTheme)
     localStorage.setItem('initLang', initLang)
+    localStorage.setItem('userConfig', userConfig)
   }
   return state
 }
