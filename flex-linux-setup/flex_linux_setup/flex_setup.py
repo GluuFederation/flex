ef51e0f26a7be83a6689709c38434c0bbd7d0c91
Devrim <mbaser@mail.com>
2022-10-25 13:42:31 +0300

    fix: flex linux setup admin UI client (#542)
    * feat: flex-linux-setup seperate Admin UI client
    
    * fix: flex-linux-setup create Admin UI client if not exists
    
    * fix: flex-linux-setup keep admin-ui configuration upon removal -- needed by CLI
    
    * fix: flex-linux-setup delete admin-ui client  upon removal
    
    * fix: flex-linux-setup plain client password in auiConfiguration.properties

--- flex-linux-setup/flex_linux_setup/flex_setup.py
+++ flex-linux-setup/flex_linux_setup/flex_setup.py
@@ -152,6 +152,7 @@ sys.path.insert(0, base.pylib_dir)
 sys.path.insert(0, os.path.join(base.pylib_dir, 'gcs'))
 
 from setup_app.pylib.jproperties import Properties
+from setup_app.pylib.ldif4.ldif import LDIFWriter
 from setup_app.utils.package_utils import packageUtils
 from setup_app.config import Config
 from setup_app.utils.collect_properties import CollectProperties
@@ -162,6 +163,8 @@ from setup_app.installers.jetty import JettyInstaller
 from setup_app.installers.jans_auth import JansAuthInstaller
 from setup_app.installers.jans_cli import JansCliInstaller
 from setup_app.utils.properties_utils import propertiesUtils
+from setup_app.utils.ldif_utils import myLdifParser
+
 
 Config.outputFolder = os.path.join(__STATIC_SETUP_DIR__, 'output')
 if not os.path.join(Config.outputFolder):
@@ -355,6 +358,27 @@ class flex_installer(JettyInstaller):
         base.extract_from_zip(self.flex_path, 'admin-ui', self.source_dir)
 
         print("Source directory:", self.source_dir)
+
+        print("Creating Gluu Flex Admin UI Client")
+
+        client_check_result = config_api_installer.check_clients([('admin_ui_client_id', '2001.')])
+        if client_check_result['2001.'] == -1:
+            ldif_parser = myLdifParser(jans_cli_installer.ldif_client)
+            ldif_parser.parse()
+
+            ldif_parser.entries[0][1]['inum'] = ['%(admin_ui_client_id)s']
+            ldif_parser.entries[0][1]['jansClntSecret'] = ['%(admin_ui_client_encoded_pw)s']
+            ldif_parser.entries[0][1]['displayName'] = ['Gluu Flex Admin UI Client']
+
+            client_tmp_fn = os.path.join(self.templates_dir, 'admin_ui_client.ldif')
+
+            with open(client_tmp_fn, 'wb') as w:
+                ldif_writer = LDIFWriter(w)
+                ldif_writer.unparse('inum=%(admin_ui_client_id)s,ou=clients,o=jans', ldif_parser.entries[0][1])
+
+            config_api_installer.renderTemplateInOut(client_tmp_fn, self.templates_dir, self.source_dir)
+            self.dbUtils.import_ldif([os.path.join(self.source_dir, os.path.basename(client_tmp_fn))])
+
         env_tmp = os.path.join(self.source_dir, '.env.tmp')
         print("env_tmp", env_tmp)
         config_api_installer.renderTemplateInOut(env_tmp, self.source_dir, self.source_dir)
@@ -373,9 +397,9 @@ class flex_installer(JettyInstaller):
         config_api_installer.copy_tree(os.path.join(self.source_dir, 'dist'),  Config.templateRenderingDict['admin_ui_apache_root'])
 
         Config.templateRenderingDict['adminui_authentication_mode'] = argsp.adminui_authentication_mode
-        config_api_installer.check_clients([('role_based_client_id', '2000.')])
-        config_api_installer.renderTemplateInOut(self.admin_ui_config_properties_path, os.path.join(self.flex_setup_dir, 'templates'), config_api_installer.custom_config_dir)
-        
+
+        config_api_installer.renderTemplateInOut(self.admin_ui_config_properties_path, self.templates_dir, config_api_installer.custom_config_dir)
+
         config_api_installer.copyFile(self.admin_ui_plugin_source_path, config_api_installer.libDir)
         config_api_installer.add_extra_class(self.admin_ui_plugin_path)
 
@@ -605,8 +629,10 @@ class flex_installer(JettyInstaller):
     def uninstall_admin_ui(self):
         print("Uninstalling Gluu Admin-UI")
 
-        print("  - Deleting admin-ui configuration from db backend")
-        self.dbUtils.delete_dn('ou=admin-ui,ou=configuration,o=jans')
+        client_check_result = config_api_installer.check_clients([('admin_ui_client_id', '2001.')])
+        if client_check_result['2001.'] == 1:
+            print("  - Deleting Gluu Flex Admin UI Client ", Config.admin_ui_client_id)
+            self.dbUtils.delete_dn('inum={},ou=clients,o=jans'.format(Config.admin_ui_client_id))
 
         print("  - Removing Admin UI directives from apache configuration")
         self.remove_apache_directive('<Directory "{}">'.format(Config.templateRenderingDict['admin_ui_apache_root']))
@@ -614,7 +640,6 @@ class flex_installer(JettyInstaller):
         if os.path.exists(self.admin_ui_plugin_path):
             print(del_msg, self.admin_ui_plugin_path)
             self.run(['rm', '-f', self.admin_ui_plugin_path])
-
 
         write_config_api_xml = False
         config_api_plugins = config_api_installer.get_plugins(paths=True)
