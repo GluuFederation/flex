97264562d307e457aeb8a527db8508012f5b1640
Harry Andriyan <harry.andriyan@gmail.com>
2022-11-07 15:54:26 +0700

    feat(admin-ui): code formating adjustment

--- admin-ui/plugins/services/redux/api/index.js
+++ admin-ui/plugins/services/redux/api/index.js
@@ -1,7 +1,7 @@
-import axios from "axios";
+import axios from "axios"
 
 export const baseUrl = process.env.API_BASE_URL || "http://localhost:8080";
 export default axios.create({
   baseURL: baseUrl,
   timeout: 30000
-});
+})
