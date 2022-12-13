e97a94ea819a4c74a490062bf9fa2071ed3843d3
Jatin Mehta <mjatin78@gmail.com>
2022-12-13 11:42:23 +0530

    fix(admin-ui): remove toast on search

--- admin-ui/plugins/schema/redux/sagas/AttributeSaga.js
+++ admin-ui/plugins/schema/redux/sagas/AttributeSaga.js
@@ -61,11 +61,9 @@ export function* searchAttributes({ payload }) {
     addAdditionalData(audit, FETCH, PERSON_SCHEMA, payload)
     const attributeApi = yield* newFunction()
     const data = yield call(attributeApi.searchAttributes, payload.options)
-    yield put(updateToast(true, 'success'))
     yield put(getAttributesResponse(data))
     yield call(postUserAction, audit)
   } catch (e) {
-    yield put(updateToast(true, 'error'))
     yield put(getAttributesResponse(null))
     if (isFourZeroOneError(e)) {
       const jwt = yield select((state) => state.authReducer.userinfo_jwt)
