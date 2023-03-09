835a13f2a89017fda00a3a3c34ad2741cbbbf1e9
moabu <47318409+moabu@users.noreply.github.com>
2023-03-09 11:42:58 +0000

    chore(main): switch to dev and snapshot

--- flex-linux-setup/flex_linux_setup/flex_setup.py
+++ flex-linux-setup/flex_linux_setup/flex_setup.py
@@ -242,10 +242,10 @@ app_versions = {
   "SETUP_BRANCH": argsp.jans_setup_branch,
   "FLEX_BRANCH": argsp.flex_branch,
   "JANS_BRANCH": argsp.jans_branch,
-  "JANS_APP_VERSION": "1.0.9",
-  "JANS_BUILD": "",
+  "JANS_APP_VERSION": "1.0.10",
+  "JANS_BUILD": "-SNAPSHOT",
   "NODE_VERSION": "v14.18.2",
-  "CASA_VERSION": "5.0.0-9",
+  "CASA_VERSION": "5.0.0-SNAPSHOT",
   "NODE_MODULES_BRANCH": argsp.node_modules_branch or argsp.flex_branch
 }
 
