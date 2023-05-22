0ff6c9fb92782d5062e4f6acfd0f153b6d7d5670
moabu <47318409+moabu@users.noreply.github.com>
2023-05-22 15:56:45 +0000

    ci: fix checksum generation

--- automation/packaging/rpm/el8/checksum.sh
+++ automation/packaging/rpm/el8/checksum.sh
@@ -5,7 +5,7 @@ REL=$(echo "%VERSION%" | sed "s/^${VERSION}//g" | sed "s/^-//g")
 if [ -z "$REL" ]; then
         RELEASE="el8"
 else
-        RELEASE="$REL.el8"
+        RELEASE="$REL-el8"
 fi
 pushd rpmbuild/RPMS/x86_64
 sha256sum flex-$VERSION-$RELEASE.x86_64.rpm > flex-$VERSION-$RELEASE.x86_64.rpm.sha256sum
