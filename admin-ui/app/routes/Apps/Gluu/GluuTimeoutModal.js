cf84ffbecccf17c68a4c1c2402c80c659d53f150
Jeet Viramgama <jviramgama5@gmail.com>
2023-07-10 13:18:26 +0530

    fix(admin-ui): add an action to refresh the page on timeout

--- admin-ui/app/routes/Apps/Gluu/GluuTimeoutModal.js
+++ admin-ui/app/routes/Apps/Gluu/GluuTimeoutModal.js
@@ -1,13 +1,28 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useContext } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useDispatch, useSelector } from 'react-redux'
 import { Button, Modal, ModalBody, ModalFooter } from 'reactstrap'
 import { handleApiTimeout } from 'Redux/features/initSlice'
+import { ThemeContext } from 'Context/theme/themeContext'
 
 const GluuTimeoutModal = ({ description = '' }) => {
   const dispatch = useDispatch()
   const { t } = useTranslation()
   const { isTimeout } = useSelector((state) => state.initReducer)
+  const { authServerHost } = useSelector((state) => state.authReducer.config)
+  const theme = useContext(ThemeContext)
+  const selectedTheme = theme.state.theme
+
+  const handleRefresh = () => {
+    dispatch(handleApiTimeout({ isTimeout: false }))
+    
+    const host = authServerHost ? `${authServerHost}/admin` : null
+    if(host) {
+      window.location.href = host
+    } else {
+      window.location.reload()
+    }
+  };
 
   const handler = useCallback(() => {
     dispatch(handleApiTimeout({ isTimeout: false }))
@@ -25,7 +40,8 @@ const GluuTimeoutModal = ({ description = '' }) => {
         <p>{description}</p>
       </ModalBody>
       <ModalFooter>
-        <Button onClick={handler}>{t('actions.ok')}</Button>
+        <Button onClick={handler}>{t('actions.close')}</Button>
+        <Button onClick={handleRefresh} color={`primary-${selectedTheme}`}>{t('actions.try_again')}</Button>
       </ModalFooter>
     </Modal>
   )
