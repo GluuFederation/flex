4df9fe2834ef8e299b52d91ef616fd4a35bd12d6
Jeet Viramgama <jviramgama5@gmail.com>
2023-07-12 17:15:27 +0530

    fix(admin-ui): remove agama developer studio #1182

--- admin-ui/plugins/auth-server/redux/api/AgamaApi.js
+++ admin-ui/plugins/auth-server/redux/api/AgamaApi.js
@@ -8,7 +8,7 @@ export default class AgamaApi {
   // Get Agama
   getAgama = (payload) => {
     return new Promise((resolve, reject) => {
-      this.api.getAgamaDevPrj(payload, (error, data) => {
+      this.api.getAgamaPrj(payload, (error, data) => {
         handleResponse(error, reject, resolve, data)
       })
     })
@@ -33,7 +33,7 @@ export default class AgamaApi {
   }
   deleteAgama = ({payload}) => {
     return new Promise((resolve, reject) => {
-      this.api.deleteAgamaDevStudioPrj(payload.name, (error, data) => {
+      this.api.deleteAgamaPrj(payload.name, (error, data) => {
         handleResponse(error, reject, resolve, data)
       })
     })
