ab2179aa8741064931e3a843ba3836a3513e2f94
moabu <47318409+moabu@users.noreply.github.com>
2023-07-14 08:34:39 +0000

    chore: 5.0.0-SNAPSHOT to 5.0.0-15

--- flex-linux-setup/flex_linux_setup/flex_setup.py
+++ flex-linux-setup/flex_linux_setup/flex_setup.py
@@ -259,7 +259,7 @@ app_versions = {
   "JANS_APP_VERSION": "1.0.15",
   "JANS_BUILD": "-SNAPSHOT",
   "NODE_VERSION": "v18.16.0",
-  "CASA_VERSION": "5.0.0-SNAPSHOT",
+  "CASA_VERSION": "5.0.0-15",
   "NODE_MODULES_BRANCH": argsp.node_modules_branch or argsp.flex_branch
 }
 
