f265fcf4ad7a12e2a426a4e17cb50efb6de9ad5c
Jeet Viramgama <jviramgama5@gmail.com>
2023-09-19 17:26:13 +0530

    feat(admin-ui): implement navigation path for tab view in a page #1333
    Signed-off-by: Jeet Viramgama <jviramgama5@gmail.com>

--- admin-ui/plugins/fido/plugin-metadata.js
+++ admin-ui/plugins/fido/plugin-metadata.js
@@ -1,6 +1,6 @@
 import { reducer as fidoReducer } from './redux/features/fidoSlice'
 import fidoSaga from './redux/sagas/FidoSaga'
-import { FIDO_READ, FIDO_WRITE } from '../../app/utils/PermChecker'
+import { FIDO_READ, FIDO_WRITE } from 'Utils/PermChecker'
 import Fido from './components/Fido'
 
 const PLUGIN_BASE_PATH = '/fido'
@@ -18,6 +18,16 @@ const pluginMetadata = {
     {
       component: Fido,
       path: PLUGIN_BASE_PATH + '/fidomanagement',
+      permission: FIDO_WRITE,
+    },
+    {
+      component: Fido,
+      path: PLUGIN_BASE_PATH + '/fidomanagement' + '/static-configuration',
+      permission: FIDO_WRITE,
+    },
+    {
+      component: Fido,
+      path: PLUGIN_BASE_PATH + '/fidomanagement' + '/dynamic-configuration',
       permission: FIDO_WRITE,
     }
   ],
