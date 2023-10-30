5a73a16c2a01e27c55d4eff2fa97116c93483584
moabu <47318409+moabu@users.noreply.github.com>
2023-10-30 15:42:05 +0300

    ci: add check for jans auth xml

--- docker-flex-all-in-one/app/bin/flex-entrypoint.sh
+++ docker-flex-all-in-one/app/bin/flex-entrypoint.sh
@@ -20,7 +20,16 @@ check_auth_custom_libs_dir() {
     done
 }
 
+check_jans_auth_xml() {
+    host=$(hostname -f)
+    while true; do
+        echo "[I] Checking contents of ${host}:/opt/jans/jetty/jans-auth/webapps/jans-auth.xml"
+        cat /opt/jans/jetty/jans-auth/webapps/jans-auth.xml;
+        sleep 10
+    done
+}
 check_agama_scripts_dir &
 check_auth_custom_libs_dir &
+check_jans_auth_xml &
 
 sh /app/bin/entrypoint.sh
