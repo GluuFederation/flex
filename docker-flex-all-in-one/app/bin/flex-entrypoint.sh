3bc62abe10612c96b6b35cf72780c1ecdaa4bba4
iromli <isman.firmansyah@gmail.com>
2023-10-31 18:19:14 +0700

    chore: remove debug functions

--- docker-flex-all-in-one/app/bin/flex-entrypoint.sh
+++ docker-flex-all-in-one/app/bin/flex-entrypoint.sh
@@ -2,34 +2,5 @@
 
 set -e
 
-check_agama_scripts_dir() {
-    host=$(hostname -f)
-    while true; do
-        echo "[I] Checking contents of ${host}:/opt/jans/jetty/jans-auth/agama/scripts directory"
-        ls -l /opt/jans/jetty/jans-auth/agama/scripts;
-        sleep 10
-    done
-}
-
-check_auth_custom_libs_dir() {
-    host=$(hostname -f)
-    while true; do
-        echo "[I] Checking contents of ${host}:/opt/jans/jetty/jans-auth/custom/libs directory"
-        ls -l /opt/jans/jetty/jans-auth/custom/libs;
-        sleep 10
-    done
-}
-
-check_jans_auth_xml() {
-    host=$(hostname -f)
-    while true; do
-        echo "[I] Checking contents of ${host}:/opt/jans/jetty/jans-auth/webapps/jans-auth.xml"
-        cat /opt/jans/jetty/jans-auth/webapps/jans-auth.xml;
-        sleep 10
-    done
-}
-check_agama_scripts_dir &
-check_auth_custom_libs_dir &
-check_jans_auth_xml &
-
+# execute Jans all-in-one entrypoint
 sh /app/bin/entrypoint.sh
