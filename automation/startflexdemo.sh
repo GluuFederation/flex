b47ccb42e0b2b69aafaf69ec7ab69c72fb544251
Amro Misbah <amromisba7@gmail.com>
2023-11-02 21:45:53 +0200

    docs: fix local flex deployment and create a script
    Signed-off-by: Amro Misbah <amromisba7@gmail.com>

---
+++ automation/startflexdemo.sh
@@ -0,0 +1,277 @@
+#!/bin/bash
+set -eo pipefail
+GLUU_FQDN=$1
+GLUU_PERSISTENCE=$2
+GLUU_LICENSE_SSA=$3
+GLUU_CI_CD_RUN=$4
+EXT_IP=$5
+INSTALL_ISTIO=$5
+if [[ ! "$GLUU_FQDN" ]]; then
+  read -rp "Enter Hostname [demoexample.gluu.org]:                           " GLUU_FQDN
+fi
+if ! [[ $GLUU_FQDN == *"."*"."* ]]; then
+  echo "[E] Hostname provided is invalid or empty.
+    Please enter a FQDN with the format demoexample.gluu.org"
+  exit 1
+fi
+if [[ ! "$GLUU_PERSISTENCE" ]]; then
+  read -rp "Enter persistence type [LDAP|MYSQL]:                            " GLUU_PERSISTENCE
+fi
+if [[ $GLUU_PERSISTENCE != "LDAP" ]] && [[ $GLUU_PERSISTENCE != "MYSQL" ]]; then
+  echo "[E] Incorrect entry. Please enter either LDAP or MYSQL"
+  exit 1
+fi
+if [[ ! "$GLUU_LICENSE_SSA" ]]; then
+  read -rp "Enter base64 encoded licenseSsa:                           " GLUU_LICENSE_SSA
+fi
+LOG_TARGET="FILE"
+LOG_LEVEL="TRACE"
+if [[ -z $GLUU_CI_CD_RUN ]]; then
+  LOG_TARGET="STDOUT"
+  LOG_LEVEL="INFO"
+fi
+
+if [[ -z $EXT_IP ]]; then
+  EXT_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+fi
+
+sudo apt-get update
+sudo apt-get install openssl -y
+sudo apt-get install python3-pip -y
+sudo pip3 install pip --upgrade
+sudo pip3 install setuptools --upgrade
+sudo pip3 install pyOpenSSL --upgrade
+sudo apt-get update
+sudo apt-get install build-essential unzip -y
+sudo pip3 install requests --upgrade
+sudo pip3 install shiv
+sudo snap install microk8s --classic
+sudo microk8s.status --wait-ready
+sudo microk8s.enable dns registry ingress hostpath-storage
+sudo microk8s kubectl get daemonset.apps/nginx-ingress-microk8s-controller -n ingress -o yaml | sed -s "s@ingress-class=public@ingress-class=nginx@g" | microk8s kubectl apply -f -
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install net-tools
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+sudo microk8s config | sudo tee ~/.kube/config > /dev/null
+sudo snap alias microk8s.kubectl kubectl
+KUBECONFIG=~/.kube/config
+sudo microk8s.kubectl create namespace gluu --kubeconfig="$KUBECONFIG" || echo "namespace exists"
+
+if [[ $INSTALL_ISTIO == "true" ]]; then
+  sudo microk8s.kubectl label ns gluu istio-injection=enabled
+  sudo curl -L https://istio.io/downloadIstio | sh -
+  cd istio-*
+  export PATH=$PWD/bin:$PATH
+  sudo ./bin/istioctl install --set profile=demo -y
+  cd ..
+fi
+
+PERSISTENCE_TYPE="sql"
+if [[ $GLUU_PERSISTENCE == "MYSQL" ]]; then
+  sudo helm repo add bitnami https://charts.bitnami.com/bitnami
+  sudo microk8s.kubectl get po --kubeconfig="$KUBECONFIG"
+  sudo helm install my-release --set auth.rootPassword=Test1234#,auth.database=gluu bitnami/mysql -n gluu --kubeconfig="$KUBECONFIG"
+  cat << EOF > override.yaml
+config:
+  countryCode: US
+  email: support@gluu.org
+  orgName: Gluu
+  city: Austin
+  configmap:
+    cnSqlDbName: gluu
+    cnSqlDbPort: 3306
+    cnSqlDbDialect: mysql
+    cnSqlDbHost: my-release-mysql.gluu.svc
+    cnSqlDbUser: root
+    cnSqlDbTimezone: UTC
+    cnSqldbUserPassword: Test1234#
+EOF
+fi
+
+ENABLE_LDAP="false"
+if [[ $GLUU_PERSISTENCE == "LDAP" ]]; then
+  openssl req \
+    -x509 \
+    -newkey rsa:2048 \
+    -sha256 \
+    -days 365 \
+    -nodes \
+    -keyout opendj.key \
+    -out opendj.crt \
+    -subj "/CN=$GLUU_FQDN" \
+    -addext 'subjectAltName=DNS:ldap,DNS:opendj'
+
+  LDAP_CERT_B64=$(base64 opendj.crt -w0)
+  LDAP_KEY_B64=$(base64 opendj.key -w0)
+
+  rm -f opendj.crt opendj.key
+
+  cat << EOF > override.yaml
+config:
+  countryCode: US
+  email: support@gluu.org
+  orgName: Gluu
+  city: Austin
+  configmap:
+    cnLdapCrt: $LDAP_CERT_B64
+    cnLdapKey: $LDAP_KEY_B64
+EOF
+  PERSISTENCE_TYPE="ldap"
+  ENABLE_LDAP="true"
+fi
+
+echo "$EXT_IP $GLUU_FQDN" | sudo tee -a /etc/hosts > /dev/null
+cat << EOF >> override.yaml
+global:
+  licenseSsa: $GLUU_LICENSE_SSA
+  cloud:
+    testEnviroment: true
+  istio:
+    enable: $INSTALL_ISTIO
+  cnPersistenceType: $PERSISTENCE_TYPE
+  admin-ui:
+    ingress:
+      adminUiEnabled: true
+  auth-server-key-rotation:
+    enabled: true
+  auth-server:
+    appLoggers:
+      authLogTarget: "$LOG_TARGET"
+      authLogLevel: "$LOG_LEVEL"
+      httpLogTarget: "$LOG_TARGET"
+      httpLogLevel: "$LOG_LEVEL"
+      persistenceLogTarget: "$LOG_TARGET"
+      persistenceLogLevel: "$LOG_LEVEL"
+      persistenceDurationLogTarget: "$LOG_TARGET"
+      persistenceDurationLogLevel: "$LOG_LEVEL"
+      ldapStatsLogTarget: "$LOG_TARGET"
+      ldapStatsLogLevel: "$LOG_LEVEL"
+      scriptLogTarget: "$LOG_TARGET"
+      scriptLogLevel: "$LOG_LEVEL"
+      auditStatsLogTarget: "$LOG_TARGET"
+      auditStatsLogLevel: "$LOG_LEVEL"
+  casa:
+    # -- App loggers can be configured to define where the logs will be redirected to and the level of each in which it should be displayed.
+    appLoggers:
+      casaLogTarget: "$LOG_TARGET"
+      casaLogLevel: "$LOG_LEVEL"
+      timerLogTarget: "$LOG_TARGET"
+      timerLogLevel: "$LOG_LEVEL"
+    enabled: true
+    ingress:
+      casaEnabled: true    
+  config-api:
+    enabled: true
+    appLoggers:
+      configApiLogTarget: "$LOG_TARGET"
+      configApiLogLevel: "$LOG_LEVEL"
+    adminUiAppLoggers:
+      # -- config-api admin-ui plugin log level
+      adminUiLogTarget: "$LOG_TARGET"
+      adminUiLogLevel: "$LOG_LEVEL"
+      adminUiAuditLogTarget: "$LOG_TARGET"
+      adminUiAuditLogLevel: "$LOG_LEVEL"
+  fido2:
+    enabled: true
+    ingress:
+      fido2ConfigEnabled: true
+    appLoggers:
+      fido2LogTarget: "$LOG_TARGET"
+      fido2LogLevel: "$LOG_LEVEL"
+      persistenceLogTarget: "$LOG_TARGET"
+      persistenceLogLevel: "$LOG_LEVEL"
+  scim:
+    enabled: true
+    ingress:
+      scimConfigEnabled: true
+      scimEnabled: true
+    appLoggers:
+      scimLogTarget: "$LOG_TARGET"
+      scimLogLevel: "$LOG_LEVEL"
+      persistenceLogTarget: "$LOG_TARGET"
+      persistenceLogLevel: "$LOG_LEVEL"
+      persistenceDurationLogTarget: "$LOG_TARGET"
+      persistenceDurationLogLevel: "$LOG_LEVEL"
+      ldapStatsLogTarget: "$LOG_TARGET"
+      ldapStatsLogLevel: "$LOG_LEVEL"
+      scriptLogTarget: "$LOG_TARGET"
+      scriptLogLevel: "$LOG_LEVEL"
+  fqdn: $GLUU_FQDN
+  isFqdnRegistered: false
+  lbIp: $EXT_IP
+  opendj:
+    # -- Boolean flag to enable/disable the OpenDJ  chart.
+    enabled: $ENABLE_LDAP
+# -- Nginx ingress definitions chart
+nginx-ingress:
+  ingress:
+    openidConfigEnabled: true
+    uma2ConfigEnabled: true
+    webfingerEnabled: true
+    webdiscoveryEnabled: true
+    scimConfigEnabled: true
+    scimEnabled: true
+    configApiEnabled: true
+    u2fConfigEnabled: true
+    fido2ConfigEnabled: true
+    authServerEnabled: true
+    path: /
+    hosts:
+    - $GLUU_FQDN
+    # -- Secrets holding HTTPS CA cert and key.
+    tls:
+    - secretName: tls-certificate
+      hosts:
+      - $GLUU_FQDN
+auth-server:
+  livenessProbe:
+    # https://github.com/JanssenProject/docker-jans-auth-server/blob/master/scripts/healthcheck.py
+    exec:
+      command:
+        - python3
+        - /app/scripts/healthcheck.py
+    # Setting for testing purposes only. Under optimal resources the app should be up in 30-60 secs
+    initialDelaySeconds: 300
+    periodSeconds: 30
+    timeoutSeconds: 5
+  readinessProbe:
+    exec:
+      command:
+        - python3
+        - /app/scripts/healthcheck.py
+    # Setting for testing purposes only. Under optimal resources the app should be up in 30-60 secs
+    initialDelaySeconds: 300
+    periodSeconds: 25
+    timeoutSeconds: 5
+opendj:
+  image:
+    repository: gluufederation/opendj
+    tag: 5.0.0_dev
+EOF
+sudo helm repo add gluu-flex https://docs.gluu.org/charts
+sudo helm repo update
+sudo helm install gluu gluu-flex/gluu -n gluu -f override.yaml --kubeconfig="$KUBECONFIG"
+echo "Waiting for auth-server to come up. This may take 5-10 mins....Please do not cancel out...This will wait for the auth-server to be ready.."
+sleep 300
+cat << EOF > testendpoints.sh
+sudo microk8s config > config
+KUBECONFIG="$PWD"/config
+sleep 10
+echo -e "Testing openid-configuration endpoint.. \n"
+curl -k https://$GLUU_FQDN/.well-known/openid-configuration
+echo -e "Testing scim-configuration endpoint.. \n"
+curl -k https://$GLUU_FQDN/.well-known/scim-configuration
+echo -e "Testing fido2-configuration endpoint.. \n"
+curl -k https://$GLUU_FQDN/.well-known/fido2-configuration
+cd ..
+EOF
+sudo microk8s.kubectl -n gluu wait --for=condition=available --timeout=300s deploy/gluu-auth-server --kubeconfig="$KUBECONFIG" || echo "Couldn't find deployment running tests anyways..."
+sudo bash testendpoints.sh
+echo -e "You may re-execute bash testendpoints.sh to do a quick test to check the openid-configuration endpoint."
