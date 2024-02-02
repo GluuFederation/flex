13466a3280006d2d8bd1b6bae541b051491468e3
iromli <isman.firmansyah@gmail.com>
2024-02-03 03:53:49 +0700

    fix: handle missing jansRevision attribute
    Signed-off-by: iromli <isman.firmansyah@gmail.com>

--- docker-admin-ui/scripts/bootstrap.py
+++ docker-admin-ui/scripts/bootstrap.py
@@ -176,7 +176,7 @@ class PersistenceSetup:
             if should_update:
                 logger.info("Updating admin-ui config app")
                 entry["jansConfApp"] = json.dumps(merged_conf)
-                entry["jansRevision"] += 1
+                entry["jansRevision"] = entry.get("jansRevision", 0) + 1
                 self.client.update(table_name, dn, entry)
 
         elif self.persistence_type == "couchbase":
