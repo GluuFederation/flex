6591aea788756d80e70e1ad93ae873658c7161a7
Jeet Viramgama <jviramgama5@gmail.com>
2024-05-17 09:16:42 +0300

    fix(admin-ui): sonarcloud security hotspot
    Signed-off-by: Jeet Viramgama <jviramgama5@gmail.com>

--- admin-ui/plugins/admin/components/Webhook/WebhookURLChecker.js
+++ admin-ui/plugins/admin/components/Webhook/WebhookURLChecker.js
@@ -1,16 +1,11 @@
 const NOT_ALLOWED = ["http://", "ftp://", "file://", "telnet://", "smb://", "ssh://", "ldap://", "https://192.168", "https://127.0", "https://172", "https://localhost"]
+const PATTERN = /^(https?:\/\/)?([\w-]+\.)+[\w-]+(:\d+)?(\/[\w-.]*)*$/i;
+
 export const isValid = (url) => {
     if (url === undefined || url === null || !isAllowed(url)) {
         return false;
     } else {
-        const exp = '^(https?:\\/\\/)?' + // protocol
-            '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-            '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-            '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-            '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-            '(\\#[-a-z\\d_]*)?$';
-        const pattern = new RegExp(exp, 'i');
-        return pattern.test(url)
+        return PATTERN.test(url)
     }
 }
 
