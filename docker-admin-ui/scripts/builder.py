fb8d18104af1098401d41d07634ad3ba8cd3b1f2
iromli <isman.firmansyah@gmail.com>
2024-05-29 05:35:15 +0700

    chore: reuse pre-built admin-ui app
    Signed-off-by: iromli <isman.firmansyah@gmail.com>

--- docker-admin-ui/scripts/builder.py
+++ docker-admin-ui/scripts/builder.py
@@ -1,12 +1,12 @@
 import logging.config
-import json
+# import json
 import os
 import shutil
-import sys
+# import sys
 
-import sh
+# import sh
 
-from jans.pycloudlib.utils import exec_cmd
+# from jans.pycloudlib.utils import exec_cmd
 
 from settings import LOGGING_CONFIG
 
@@ -16,71 +16,80 @@ logger = logging.getLogger("admin-ui")
 ADMIN_UI_DIR = "/opt/flex/admin-ui"
 
 
-def _emit_to_log(line, stdin, process):
-    line = line.strip().replace("\n", "")
-    if line:
-        logger.info(line)
+# def _emit_to_log(line, stdin, process):
+#     line = line.strip().replace("\n", "")
+#     if line:
+#         logger.info(line)
 
 
-def _discover_plugins():
-    os.chdir(ADMIN_UI_DIR)
+# def _discover_plugins():
+#     os.chdir(ADMIN_UI_DIR)
 
-    user_plugins = [
-        plugin.strip()
-        for plugin in os.environ.get("GLUU_ADMIN_UI_PLUGINS", "").strip().split(",")
-        if plugin.strip()
-    ]
+#     user_plugins = [
+#         plugin.strip()
+#         for plugin in os.environ.get("GLUU_ADMIN_UI_PLUGINS", "").strip().split(",")
+#         if plugin.strip()
+#     ]
 
-    for plugin in set(user_plugins):
-        src = f"/opt/flex/admin-ui/_plugins/{plugin}.zip"
+#     for plugin in set(user_plugins):
+#         src = f"/opt/flex/admin-ui/_plugins/{plugin}.zip"
 
-        if not os.path.isfile(src):
-            continue
+#         if not os.path.isfile(src):
+#             continue
 
-        logger.info(f"Adding plugin {plugin} from {src}")
-        cmd = f"npm run plugin:add {src}"
-        out, err, code = exec_cmd(cmd)
+#         logger.info(f"Adding plugin {plugin} from {src}")
+#         cmd = f"npm run plugin:add {src}"
+#         out, err, code = exec_cmd(cmd)
 
-        if code != 0:
-            sys.exit(err.decode())
-        logger.info(out.decode())
+#         if code != 0:
+#             sys.exit(err.decode())
+#         logger.info(out.decode())
 
-    with open(os.path.join(ADMIN_UI_DIR, "plugins.config.json")) as f:
-        return [plug["key"] for plug in json.loads(f.read())]
+#     with open(os.path.join(ADMIN_UI_DIR, "plugins.config.json")) as f:
+#         return [plug["key"] for plug in json.loads(f.read())]
 
 
-def _build_src() -> None:
-    logger.info("Building admin-ui app ...")
-    public_dir = os.environ.get("GLUU_ADMIN_UI_PUBLIC_DIR", "/var/lib/nginx/html")
+# def _build_src() -> None:
+#     logger.info("Building admin-ui app ...")
+#     public_dir = os.environ.get("GLUU_ADMIN_UI_PUBLIC_DIR", "/var/lib/nginx/html")
 
-    os.chdir(ADMIN_UI_DIR)
+#     os.chdir(ADMIN_UI_DIR)
 
-    try:
-        err = ""
-        proc = sh.npm("run", "build:prod", _out=_emit_to_log, _err_to_out=True, _bg=True, _truncate_exc=False)
-        proc.wait()
-    except sh.SignalException_SIGKILL:
-        err = "Process is killed"
-    except sh.ErrorReturnCode as exc:
-        err = exc.stderr.decode() or "Error running command"
+#     try:
+#         err = ""
+#         proc = sh.npm("run", "build:prod", _out=_emit_to_log, _err_to_out=True, _bg=True, _truncate_exc=False)
+#         proc.wait()
+#     except sh.SignalException_SIGKILL:
+#         err = "Process is killed"
+#     except sh.ErrorReturnCode as exc:
+#         err = exc.stderr.decode() or "Error running command"
 
-    if proc.exit_code != 0:
-        err = err or proc.stderr.decode()
-        logger.error(f"Unable to build admin-ui app; exit code={proc.exit_code}, reason={err}")
-        sys.exit(proc.exit_code)
+#     if proc.exit_code != 0:
+#         err = err or proc.stderr.decode()
+#         logger.error(f"Unable to build admin-ui app; exit code={proc.exit_code}, reason={err}")
+#         sys.exit(proc.exit_code)
 
-    shutil.copytree(
-        os.path.join(ADMIN_UI_DIR, "dist"),
-        public_dir,
-        dirs_exist_ok=True,
-    )
-    logger.info("Finished building admin-ui app")
+#     shutil.copytree(
+#         os.path.join(ADMIN_UI_DIR, "dist"),
+#         public_dir,
+#         dirs_exist_ok=True,
+#     )
+#     logger.info("Finished building admin-ui app")
+
+
+def copy_admin_ui_dist():
+    src = os.path.join(ADMIN_UI_DIR, "dist")
+    dest = os.environ.get("GLUU_ADMIN_UI_PUBLIC_DIR", "/var/lib/nginx/html")
+    logger.info(f"Copying admin-ui app from {src} to {dest}")
+    shutil.copytree(src, dest, dirs_exist_ok=True)
+    logger.info("Finished copying admin-ui app")
 
 
 def main() -> None:
-    loaded_plugins = _discover_plugins()
-    _build_src()
-    logger.info(f"Loaded plugins: {', '.join(loaded_plugins)}")
+    # loaded_plugins = _discover_plugins()
+    # _build_src()
+    # logger.info(f"Loaded plugins: {', '.join(loaded_plugins)}")
+    copy_admin_ui_dist()
 
 
 if __name__ == "__main__":
