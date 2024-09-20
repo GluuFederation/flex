692b1f92f970097aab31d9c61b41c47781d75a94
Mohammad Abudayyeh <47318409+moabu@users.noreply.github.com>
2024-09-20 04:29:11 +0000

    chore(release): prepare for 5.1.5 (#1831)
    * chore(release): prepare for 5.1.5 release triggered by https://github.com/JanssenProject/jans/pull/9499
    
    Signed-off-by: moabu <47318409+moabu@users.noreply.github.com>
    
    * ci: fix default image tags
    
    Signed-off-by: moabu <47318409+moabu@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: moabu <47318409+moabu@users.noreply.github.com>

--- flex-linux-setup/flex_linux_setup/flex_setup.py
+++ flex-linux-setup/flex_linux_setup/flex_setup.py
@@ -258,7 +258,7 @@ app_versions = {
   "FLEX_BRANCH": argsp.flex_branch,
   "JANS_BRANCH": argsp.jans_branch,
   "JANS_APP_VERSION": "1.1.5",
-  "JANS_BUILD": "-SNAPSHOT",
+  "JANS_BUILD": "",
   "NODE_VERSION": "v18.16.0",
   "NODE_MODULES_BRANCH": argsp.node_modules_branch or argsp.flex_branch
 }
