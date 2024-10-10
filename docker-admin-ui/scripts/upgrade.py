9bcc4f6404f1a7b242f2d84dc53c3820c129a229
iromli <isman.firmansyah@gmail.com>
2024-10-11 02:29:31 +0700

    feat(cloud-native): remove support for ldap persistence
    Signed-off-by: iromli <isman.firmansyah@gmail.com>

--- docker-admin-ui/scripts/upgrade.py
+++ docker-admin-ui/scripts/upgrade.py
@@ -5,7 +5,6 @@ from collections import namedtuple
 
 from jans.pycloudlib import get_manager
 from jans.pycloudlib.persistence import CouchbaseClient
-from jans.pycloudlib.persistence import LdapClient
 from jans.pycloudlib.persistence import SpannerClient
 from jans.pycloudlib.persistence import SqlClient
 from jans.pycloudlib.persistence import PersistenceMapper
@@ -19,44 +18,6 @@ logging.config.dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger("admin-ui")
 
 Entry = namedtuple("Entry", ["id", "attrs"])
-
-
-class LDAPBackend:
-    def __init__(self, manager):
-        self.manager = manager
-        self.client = LdapClient(manager)
-        self.type = "ldap"
-
-    def format_attrs(self, attrs):
-        _attrs = {}
-        for k, v in attrs.items():
-            if len(v) < 2:
-                v = v[0]
-            _attrs[k] = v
-        return _attrs
-
-    def get_entry(self, key, filter_="", attrs=None, **kwargs):
-        filter_ = filter_ or "(objectClass=*)"
-
-        entry = self.client.get(key, filter_=filter_, attributes=attrs)
-        if not entry:
-            return None
-        return Entry(entry.entry_dn, self.format_attrs(entry.entry_attributes_as_dict))
-
-    def modify_entry(self, key, attrs=None, **kwargs):
-        attrs = attrs or {}
-        del_flag = kwargs.get("delete_attr", False)
-
-        if del_flag:
-            mod = self.client.MODIFY_DELETE
-        else:
-            mod = self.client.MODIFY_REPLACE
-
-        for k, v in attrs.items():
-            if not isinstance(v, list):
-                v = [v]
-            attrs[k] = [(mod, v)]
-        return self.client.modify(key, attrs)
 
 
 class SQLBackend:
@@ -153,7 +114,6 @@ BACKEND_CLASSES = {
     "sql": SQLBackend,
     "couchbase": CouchbaseBackend,
     "spanner": SpannerBackend,
-    "ldap": LDAPBackend,
 }
 
 
