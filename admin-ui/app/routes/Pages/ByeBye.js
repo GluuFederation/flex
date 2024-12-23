92afd3da2aab0e2d87ca0b99203c39e051133edd
Arnab Dutta <arnab.bdutta@gmail.com>
2024-12-23 23:08:30 +0530

    feat: logout not working
    Signed-off-by: Arnab Dutta <arnab.bdutta@gmail.com>

--- admin-ui/app/routes/Pages/ByeBye.js
+++ admin-ui/app/routes/Pages/ByeBye.js
@@ -27,6 +27,7 @@ function ByeBye() {
   
     console.log("config: "+ JSON.stringify(config))
     if (!!config && Object.keys(config).length !== 0) {
+      console.log("Config has value"+ JSON.stringify(config));
       const state = uuidv4()
       const sessionEndpoint = `${config.endSessionEndpoint}?state=${state}&post_logout_redirect_uri=${config.postLogoutRedirectUri}`
       dispatch(logoutUser())
