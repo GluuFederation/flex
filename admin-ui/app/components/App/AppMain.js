66da7165675d8566594069b9de66b2661df69e55
Arnab Dutta <arnab.bdutta@gmail.com>
2024-12-24 00:41:52 +0530

    feat: logout not working
    Signed-off-by: Arnab Dutta <arnab.bdutta@gmail.com>

--- admin-ui/app/components/App/AppMain.js
+++ admin-ui/app/components/App/AppMain.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { configStore } from 'Redux/store'
