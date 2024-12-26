5eb9df5fbda432b8a3fbf23675eff43a940f8f5f
Arnab Dutta <arnabdutta@Arnabs-MacBook-Air.local>
2024-12-26 15:19:58 +0530

    fix: revoke token
    Signed-off-by: Arnab Dutta <arnabdutta@Arnabs-MacBook-Air.local>

--- admin-ui/app/routes/Pages/ByeBye.js
+++ admin-ui/app/routes/Pages/ByeBye.js
@@ -22,7 +22,7 @@ function ByeBye() {
         headers: {
           'Content-Type': 'application/json'
         },   body: JSON.stringify(data)
-    });
+    })
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error('Error:', error));
