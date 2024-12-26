eea56244fe86b06b7befb911694f3d36c3b502c6
Arnab Dutta <arnabdutta@Arnabs-MacBook-Air.local>
2024-12-26 14:23:58 +0530

    feat: revoke token
    Signed-off-by: Arnab Dutta <arnabdutta@Arnabs-MacBook-Air.local>

--- admin-ui/app/routes/Pages/ByeBye.js
+++ admin-ui/app/routes/Pages/ByeBye.js
@@ -7,12 +7,26 @@ import { useTranslation } from 'react-i18next'
 
 function ByeBye() {
   const config = useSelector((state) => state.authReducer.config)
+  const token = useSelector((state) => state.authReducer.token.access_token)
   const dispatch = useDispatch()
   const { t } = useTranslation()
   useEffect(() => {
     if (config) {
       const state = uuidv4()
       const sessionEndpoint = `${config.endSessionEndpoint}?state=${state}&post_logout_redirect_uri=${config.postLogoutRedirectUri}`
+      console.log('Api token: '+token)
+      const data = { token: token, token_type_hint: 'access_token' };
+
+      fetch('https://admin-ui-test.gluu.org/jans-auth/restv1/revoke', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },   body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+
       window.location.href = sessionEndpoint
     }
 
