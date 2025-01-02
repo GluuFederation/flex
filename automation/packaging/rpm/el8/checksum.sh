55315f6d43f74d6e90552e9acf16eca4bbce8ee0
moabu <47318409+moabu@users.noreply.github.com>
2025-01-02 10:42:25 +0300

    fix: release 5.2.0 Release-As: 5.2.0
    Signed-off-by: moabu <47318409+moabu@users.noreply.github.com>

--- automation/packaging/rpm/el8/checksum.sh
+++ automation/packaging/rpm/el8/checksum.sh
@@ -5,8 +5,12 @@ REL=$(echo "%VERSION%" | sed "s/^${VERSION}//g" | sed "s/^-//g")
 if [ -z "$REL" ]; then
         RELEASE="el8"
 else
-        RELEASE="$REL-el8"
+        RELEASE="$REL.el8"
 fi
 pushd rpmbuild/RPMS/x86_64
-sha256sum flex-$VERSION-$RELEASE.x86_64.rpm > flex-$VERSION-$RELEASE.x86_64.rpm.sha256sum
+
+echo "VERSION: $VERSION"
+echo "RELEASE: $RELEASE"
+echo "Creating checksum file for release build"
+sha256sum flex-"$VERSION"-"$RELEASE".x86_64.rpm > flex-"$VERSION"-"$RELEASE".x86_64.rpm.sha256sum
 popd
