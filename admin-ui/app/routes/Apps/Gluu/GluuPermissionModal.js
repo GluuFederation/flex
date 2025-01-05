ea95c89993fa08949afa1f980a9c2eeced558d00
Jatin Mehta <mjatin78@gmail.com>
2025-01-05 23:59:18 +0530

    fix(admin-ui): update token script should reject the tampered user-info-jwt

---
+++ admin-ui/app/routes/Apps/Gluu/GluuPermissionModal.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+
+const GluuPermissionModal = ({ description = "", handler, isOpen }) => {
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      <Modal
+        centered
+        isOpen={isOpen}
+        style={{ minWidth: "45vw" }}
+        toggle={handler}
+        className="modal-outline-primary"
+        backdrop="static"
+      >
+        <ModalHeader>
+          <i className="bi bi-shield-lock" /> {t("dashboard.access_denied")}
+        </ModalHeader>
+        <ModalBody className="text-center">
+          <p className="text-muted">
+            🚫 <strong>{t("dashboard.access_denied_message")}</strong>
+          </p>
+          <p>{t("dashboard.access_contact_admin")}</p>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            className="d-flex align-items-center"
+            onClick={handler}
+          >
+            {t("menus.signout")}
+          </Button>
+        </ModalFooter>
+      </Modal>
+
+      {/* Scoped CSS inside the component */}
+      <style>
+        {`
+          .modal {
+            background: #000 !important;
+          }
+        `}
+      </style>
+    </div>
+  );
+};
+
+export default GluuPermissionModal;
