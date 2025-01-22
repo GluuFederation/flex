8a134831ada1bd86a5f11a5d6044c6d30c2627fa
moabu <47318409+moabu@users.noreply.github.com>
2025-01-22 14:07:40 +0300

    chore: release 5.3.0
    Signed-off-by: moabu <47318409+moabu@users.noreply.github.com>

--- flex-linux-setup/flex_linux_setup/flex_setup.py
+++ flex-linux-setup/flex_linux_setup/flex_setup.py
@@ -267,8 +267,8 @@ app_versions = {
   "SETUP_BRANCH": argsp.jans_setup_branch,
   "FLEX_BRANCH": argsp.flex_branch,
   "JANS_BRANCH": argsp.jans_branch,
-  "JANS_APP_VERSION": "0.0.0",
-  "JANS_BUILD": "-nightly",
+  "JANS_APP_VERSION": "1.3.0",
+  "JANS_BUILD": "",
   "NODE_VERSION": "v18.16.0",
   "NODE_MODULES_BRANCH": argsp.node_modules_branch or argsp.flex_branch
 }
