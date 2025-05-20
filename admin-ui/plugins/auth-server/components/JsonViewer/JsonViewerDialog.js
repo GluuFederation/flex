ea231b0a1b7b5c00676c2b2898c8bdbca0464d1b
gasmyr <Thomas@gluu.org>
2025-05-20 15:26:58 +0100

    feat(admin-ui): add download/view feature in SSA page of Admin UI (#2068)
    feat(admin-ui): add download/view feature in SSA page of Admin UI #2063

---
+++ admin-ui/plugins/auth-server/components/JsonViewer/JsonViewerDialog.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Button } from 'reactstrap';
+import { useTranslation } from 'react-i18next';
+import JsonViewer from './JsonViewer';
+import PropTypes from 'prop-types';
+
+const JsonViewerDialog = ({
+    isOpen,
+    toggle,
+    data,
+    title = 'JSON Viewer',
+    theme = 'light',
+    expanded = true
+}) => {
+    const { t } = useTranslation();
+
+    return (
+        <Modal
+            isOpen={isOpen}
+            toggle={toggle}
+            size="lg"
+            className="modal-outline-primary"
+        >
+            <ModalHeader toggle={toggle}>
+                <i
+                    style={{ color: 'green' }}
+                    className="fa fa-2x fa-code fa-fw modal-icon mb-3"
+                />
+                {title}
+            </ModalHeader>
+            <ModalBody style={{ maxHeight: '70vh', overflow: 'auto' }}>
+                <JsonViewer
+                    data={data}
+                    theme={theme}
+                    expanded={expanded}
+                />
+            </ModalBody>
+            <ModalFooter>
+                <Button onClick={toggle}>
+                    {t('actions.close')}
+                </Button>
+            </ModalFooter>
+        </Modal>
+    );
+};
+
+JsonViewerDialog.propTypes = {
+    isOpen: PropTypes.bool.isRequired,
+    toggle: PropTypes.func.isRequired,
+    data: PropTypes.object.isRequired,
+    title: PropTypes.string,
+    theme: PropTypes.string,
+    expanded: PropTypes.bool
+};
+
+export default JsonViewerDialog;
