94c1cf777e7c39c5731166b80da3e6cd29670a5b
Faisal Siddique <71010439+faisalsiddique4400@users.noreply.github.com>
2025-05-26 20:11:41 +0500

    feat(admin-ui): hide optional components from sidebar if not installed (#2091)
    Make SCIM and FIDO components optional in sidebar based on server status
    
    Closes #2087

--- admin-ui/app/routes/Apps/Gluu/styles/GluuAppSidebar.style.js
+++ admin-ui/app/routes/Apps/Gluu/styles/GluuAppSidebar.style.js
@@ -8,7 +8,7 @@ const styles = makeStyles()({
     top: 140,
     height: 70,
     borderBottomLeftRadius: 20,
-    borderBottomRightRadius: 20,
+    borderBottomRightRadius: 20
   },
   wave: {
     position: 'relative',
@@ -19,7 +19,15 @@ const styles = makeStyles()({
     textAlign: 'center',
     position: 'relative',
     top: -130,
-    fontWeight: 500,
+    fontWeight: 500
+  },
+  waveContainerFixed: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    zIndex: 10,
+    background: 'inherit'
   }
 })
 
