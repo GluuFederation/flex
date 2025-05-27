24cdf30e5df9fb939d55c51fa75cf061c8f8235a
faisalsiddique4400 <faisalsiddique10886@gmail.com>
2025-05-27 12:10:43 +0500

    fix(admin-ui): white screen on AuthN page load #2093
    Remove obsolete Redux loading selector referencing Jans-Link plugin from AuthN component to fix render failure.

--- admin-ui/plugins/auth-server/components/AuthN/index.js
+++ admin-ui/plugins/auth-server/components/AuthN/index.js
@@ -1,47 +1,45 @@
-import React from "react";
-import AuthNListPage from "./AuthNListPage";
-import { useTranslation } from "react-i18next";
-import GluuTabs from "Routes/Apps/Gluu/GluuTabs";
-import { Card } from "Components";
-import applicationStyle from "Routes/Apps/Gluu/styles/applicationstyle";
+import React from 'react'
+import AuthNListPage from './AuthNListPage'
+import { useTranslation } from 'react-i18next'
+import GluuTabs from 'Routes/Apps/Gluu/GluuTabs'
+import { Card } from 'Components'
+import applicationStyle from 'Routes/Apps/Gluu/styles/applicationstyle'
 
-import { useSelector } from "react-redux";
-import AgamaListPage from "../Agama/AgamaListPage";
-import AliasesListPage from "../Agama/AgamaAliasListPage";
+import { useSelector } from 'react-redux'
+import AgamaListPage from '../Agama/AgamaListPage'
+import AliasesListPage from '../Agama/AgamaAliasListPage'
 
 function AuthNPage() {
-  const { t } = useTranslation();
-  const isLoading = useSelector((state) => state.cacheRefreshReducer.loading);
+  const { t } = useTranslation()
 
   const tabNames = [
     {
-      name: t("menus.builtIn"),
-      path: "",
+      name: t('menus.builtIn'),
+      path: ''
     },
-    { name: t("menus.acrs"), path: "" },
+    { name: t('menus.acrs'), path: '' },
     {
-      name: t("menus.aliases"),
-      path: "",
+      name: t('menus.aliases'),
+      path: ''
     },
     {
-      name: t("menus.agama_flows"),
-      path: "",
-    },
- 
-  ];
-
-  const tabToShow = (tabName) => {
-    switch (tabName) {
-      case t("menus.builtIn"):
-        return <AuthNListPage isBuiltIn={true}/>;
-      case t("menus.acrs"):
-        return <AuthNListPage />;
-      case t("menus.agama_flows"):
-        return <AgamaListPage />;
-      case t("menus.aliases"):
-        return <AliasesListPage />;
+      name: t('menus.agama_flows'),
+      path: ''
     }
-  };
+  ]
+
+  const tabToShow = tabName => {
+    switch (tabName) {
+      case t('menus.builtIn'):
+        return <AuthNListPage isBuiltIn={true} />
+      case t('menus.acrs'):
+        return <AuthNListPage />
+      case t('menus.agama_flows'):
+        return <AgamaListPage />
+      case t('menus.aliases'):
+        return <AliasesListPage />
+    }
+  }
 
   return (
     <Card className="mb-3" style={applicationStyle.mainCard}>
@@ -51,7 +49,7 @@ function AuthNPage() {
         withNavigation={true}
       />
     </Card>
-  );
+  )
 }
 
-export default AuthNPage;
+export default AuthNPage
