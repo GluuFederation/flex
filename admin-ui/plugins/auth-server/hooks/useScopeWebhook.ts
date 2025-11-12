610163c819aa08b715f1111613a68bdac5608c94
Mougang Thomas Gasmyr <Thomas@gluu.org>
2025-11-12 21:17:04 +0100

    fix(admin-ui): Apply code review #2433

--- admin-ui/plugins/auth-server/hooks/useScopeWebhook.ts
+++ admin-ui/plugins/auth-server/hooks/useScopeWebhook.ts
@@ -1,6 +1,7 @@
 import { useCallback } from 'react'
 import type { Scope } from 'JansConfigApi'
 import { useWebhookTrigger } from 'Plugins/admin/hooks/useWebhookTrigger'
+import { WEBHOOK_FEATURE_IDS } from 'Plugins/admin/constants/webhookFeatures'
 
 export function useScopeWebhook() {
   const trigger = useWebhookTrigger<Scope>({
@@ -9,7 +10,10 @@ export function useScopeWebhook() {
   })
 
   const triggerScopeWebhook = useCallback(
-    (scope: Scope, featureId: 'scopes_write' | 'scopes_delete'): void => {
+    (
+      scope: Scope,
+      featureId: typeof WEBHOOK_FEATURE_IDS.SCOPES_WRITE | typeof WEBHOOK_FEATURE_IDS.SCOPES_DELETE,
+    ): void => {
       trigger(scope, featureId)
     },
     [trigger],
