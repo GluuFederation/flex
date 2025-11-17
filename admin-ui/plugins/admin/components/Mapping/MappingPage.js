d7996d347442567a53c565465aedf850c2f41a31
Kiran Mali <39133739+kdhttps@users.noreply.github.com>
2025-11-17 17:53:05 +0530

    feat(admin-ui): make page UI for cedarling configuration (#2378)
    * feat: make cedarling config UI
    
    * feat: add other language translation
    
    * feat: add admin config api reducer
    
    * feat: add form validation
    
    * chore: fix format
    
    * feat: add config saga
    
    * feat: edit config
    
    * feat: use react query and remove saga
    
    * feat: update translation
    
    * feat: add toast and audit log
    
    * feat: set remote policy store as default
    
    * fix: add code rabbit changes
    
    * fix: add code rabbit changes
    
    * feat: role to scope mapping and PRP config
    
    * refactor: remove add update operation on mapping
    
    * chore: update notes
    
    * refactor: remove essential scope section
    
    * feat: update form and invalidate get query
    
    * fix: addressed PR review changes
    
    * fix: addressed PR review changes
    
    * refactor: update translations
    
    * refactor: remove role and capabilities page
    
    * feat: add mapping notes in translation
    
    * refactor: PR review fixes
    
    * docs: add cedarling config doc
    
    * docs: add policy store detaisl
    
    * docs: rephase sentence
    
    * docs: code review fixes
    
    * docs: fix spelling mistake
    
    * fix: pushed arnab review changes
    
    * feat: check remote url empty
    
    * fix: fix code review changes
    
    * fix: Remove redundant checked props
    
    * refactor: remove docs content
    
    * refactor: update review doc
    
    * fix: arnab review changes

--- admin-ui/plugins/admin/components/Mapping/MappingPage.js
+++ admin-ui/plugins/admin/components/Mapping/MappingPage.js
@@ -1,20 +1,19 @@
-import React, { useEffect, useState, useContext } from 'react'
+import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useTranslation } from 'react-i18next'
 import applicationStyle from 'Routes/Apps/Gluu/styles/applicationstyle'
-import MappingAddDialogForm from './MappingAddDialogForm'
-import { Card, Col, CardBody, FormGroup, Button } from 'Components'
-import Box from '@mui/material/Box'
+import { Card, CardBody } from 'Components'
 import GluuViewWrapper from 'Routes/Apps/Gluu/GluuViewWrapper'
 import GluuLoader from 'Routes/Apps/Gluu/GluuLoader'
-import { getMapping, addNewRolePermissions } from 'Plugins/admin/redux/features/mappingSlice'
+import { getMapping } from 'Plugins/admin/redux/features/mappingSlice'
 import { getRoles } from 'Plugins/admin/redux/features/apiRoleSlice'
 import { getPermissions } from 'Plugins/admin/redux/features/apiPermissionSlice'
 import MappingItem from './MappingItem'
 import { buildPayload, MAPPING_WRITE, MAPPING_READ } from 'Utils/PermChecker'
 import SetTitle from 'Utils/SetTitle'
-import { ThemeContext } from 'Context/theme/themeContext'
 import { useCedarling } from '@/cedarling'
+import { Link } from 'react-router-dom'
+import { StickyNote2Outlined } from '@mui/icons-material'
 
 function MappingPage() {
   const dispatch = useDispatch()
@@ -26,14 +25,8 @@ function MappingPage() {
   const apiRoles = useSelector((state) => state.apiRoleReducer.items)
   const permissionLoading = useSelector((state) => state.apiPermissionReducer.loading)
   const { permissions: cedarPermissions } = useSelector((state) => state.cedarPermissions)
-
-  const [modal, setModal] = useState(false)
-
-  const toggle = () => setModal(!modal)
   const options = []
   const userAction = {}
-  const theme = useContext(ThemeContext)
-  const selectedTheme = theme.state.theme
 
   const authorizePermissions = async () => {
     const permissions = [MAPPING_READ, MAPPING_WRITE]
@@ -60,17 +53,6 @@ function MappingPage() {
     dispatch(getPermissions({ payload: userAction }))
   }
 
-  function onAddConfirmed(mappingData) {
-    const existing = (mapping || []).find((m) => m?.role === mappingData.role)
-    const mergedPermissions = Array.from(
-      new Set([...(existing?.permissions || []), ...(mappingData?.permissions || [])]),
-    )
-    const payload = { role: mappingData.role, permissions: mergedPermissions }
-    buildPayload(userAction, 'Add new mapping', payload)
-    dispatch(addNewRolePermissions({ data: payload }))
-    toggle()
-  }
-
   function doFetchList() {
     buildPayload(userAction, 'ROLES_MAPPING', options)
     dispatch(getMapping({ action: userAction }))
@@ -80,44 +62,23 @@ function MappingPage() {
     dispatch(getRoles({ action: userAction }))
   }
 
-  function showMappingDialog() {
-    toggle()
-  }
   return (
     <GluuLoader blocking={loading || permissionLoading}>
       <Card style={applicationStyle.mainCard}>
         <CardBody>
           <GluuViewWrapper canShow={hasCedarPermission(MAPPING_READ)}>
-            {hasCedarPermission(MAPPING_WRITE) && (
-              <FormGroup row>
-                <Col sm={10}></Col>
-                <Col sm={2}>
-                  <Box display="flex" justifyContent="flex-end">
-                    <Button
-                      type="button"
-                      color={`primary-${selectedTheme}`}
-                      style={applicationStyle.buttonStyle}
-                      onClick={showMappingDialog}
-                    >
-                      <i className="fa fa-plus me-2"></i>
-                      {t('actions.add_mapping')}
-                    </Button>
-                  </Box>
-                </Col>
-              </FormGroup>
-            )}
             {mapping.map((candidate, idx) => (
               <MappingItem key={idx} candidate={candidate} roles={apiRoles} />
             ))}
           </GluuViewWrapper>
-          <FormGroup row />
-          <MappingAddDialogForm
-            roles={apiRoles}
-            handler={toggle}
-            modal={modal}
-            mapping={mapping}
-            onAccept={onAddConfirmed}
-          />
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px', marginTop: '16px' }}>
+            <StickyNote2Outlined aria-label="Note" />
+            <span>
+              {t('documentation.mappings.note_prefix')}{' '}
+              <Link to="/adm/cedarlingconfig">Cedarling</Link>{' '}
+              {t('documentation.mappings.note_suffix')}
+            </span>
+          </div>
         </CardBody>
       </Card>
     </GluuLoader>
