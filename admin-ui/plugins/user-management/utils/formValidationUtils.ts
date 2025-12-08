e3f3b89d6745b3b09c5a106cf0fdad8f31c1b516
Faisal Siddique <71010439+faisalsiddique4400@users.noreply.github.com>
2025-12-08 14:10:36 +0500

    Fix(admin-ui): roles not removable from user profile and apply action not triggered (#2496)
    * fix(admin-ui): roles not removable from user profile and apply action not triggered (#2494)
    
    * codeRabbitai suggestions
    
    * codeRabbitai suggestions
    
    * Code rabbit enhancements

---
+++ admin-ui/plugins/user-management/utils/formValidationUtils.ts
@@ -0,0 +1,27 @@
+import { ModifiedFields } from '../types/ComponentTypes'
+
+export const shouldDisableApplyButton = (
+  isSubmitting: boolean,
+  isDirty: boolean,
+  isValid: boolean,
+  modifiedFields: ModifiedFields,
+): boolean => {
+  const hasModifiedFields = Object.keys(modifiedFields).length > 0
+  const isFormChanged = isDirty || hasModifiedFields
+
+  if (isSubmitting || !isFormChanged) return true
+  if (!hasModifiedFields && !isValid) return true
+  return false
+}
+
+export const buildFormOperations = (modifiedFields: ModifiedFields) => {
+  return Object.keys(modifiedFields).map((key) => ({
+    path: key,
+    value: modifiedFields[key],
+    op: 'replace' as const,
+  }))
+}
+
+export const hasFormChanges = (isDirty: boolean, modifiedFields: ModifiedFields): boolean => {
+  return isDirty || Object.keys(modifiedFields).length > 0
+}
