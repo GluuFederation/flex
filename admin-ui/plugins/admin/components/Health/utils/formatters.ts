41e133920be3ea4151182169b9a1864a7aa0614b
Mougang Thomas Gasmyr <Thomas@gluu.org>
2025-12-09 01:51:40 +0100

    fix(admin-ui): apply code review #2503

--- admin-ui/plugins/admin/components/Health/utils/formatters.ts
+++ admin-ui/plugins/admin/components/Health/utils/formatters.ts
@@ -1,6 +1,7 @@
 export function formatServiceName(name: string): string {
   return name
-    .split(/[_-]/)
+    .split(/[_-]+/)
+    .filter((word) => word.length > 0)
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
     .join(' ')
 }
