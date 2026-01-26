191bfc5f9bdecce7e7ecdd35ac4417e149d338bb
moabu <47318409+moabu@users.noreply.github.com>
2026-01-26 12:44:07 +0200

    chore: 5.16.0 to nighly release

--- docker-flex-monolith/up.sh
+++ docker-flex-monolith/up.sh
@@ -23,7 +23,7 @@ if [ -z "$INSTALLED_FLEX_NAME" ]; then
 fi
 
 if [ -z "$FLEX_VERSION" ]; then
-    FLEX_VERSION="5.16.0-1"
+    FLEX_VERSION="0.0.0-nightly"
 fi
 
 if [ -z "$DATABASE_VOLUME_NAME" ]; then
